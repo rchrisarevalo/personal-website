a1a27ec0ab35a3cbf0cae96dd55756b514856003
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-08-11 12:09:42 -0500

    New error message added.

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import Load from "./Load";
 import Intro from "./Intro";
 import Posts from "./Posts";
 import Contact from "./Contact";
+import Error404 from "./Error404";
 
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <Route exact path="/personal-website/" component={Load}/>
-      <Route exact path ="/personal-website/about" component={Intro}/>
-      <Route exact path="/personal-website/posts" component={Posts}/>
-      <Route exact path="/personal-website/contact" component={Contact}/>
+      <Switch>
+        <Route exact path="/personal-website/" component={Load}/>
+        <Route exact path ="/personal-website/about" component={Intro}/>
+        <Route exact path="/personal-website/posts" component={Posts}/>
+        <Route exact path="/personal-website/contact" component={Contact}/>
+        <Route component={Error404}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/Error404.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+const Error404 = () => {
+    return (
+        <div className = "error-container">
+            <div className = "error-message">
+                <h1>ERROR 404!</h1>
+                <p>Oops! It looks like what you were looking for does not exist!</p>
+                <p>Go back to the <Link to="/personal-website/">Home</Link> page to reset this session.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Error404;
