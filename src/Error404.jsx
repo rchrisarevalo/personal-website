8426453d50ed77bc7568aff9eb1ab361a3414fa9
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-08-19 15:11:28 -0500

    Button display bug fix.

--- src/Error404.jsx
+++ src/Error404.jsx
@@ -8,6 +8,7 @@ const Error404 = () => {
                 <h1>ERROR 404!</h1>
                 <p>Oops! It looks like what you were looking for does not exist!</p>
                 <p>Here are some links below that can help you resolve the issue:</p>
+                <br></br>
                 <Link to="/about">About Me</Link>
                 <Link to="/posts">Posts</Link>
                 <Link to="/contact">Contact</Link>
