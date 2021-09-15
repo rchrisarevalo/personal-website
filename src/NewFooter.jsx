eadcef43d7b0a9c7a91d142f38d651b6978cfecb
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-09-14 20:29:04 -0500

    New footer info updated.

--- src/NewFooter.jsx
+++ src/NewFooter.jsx
@@ -6,7 +6,6 @@ const NewFooter = () => {
         <footer className = "new-footer">
             <div className = "new-footer-row">
                 <div className = "social-media-links" id="new-links">
-                    <li><b>Follow me on:</b></li>
                     <li><a href="https://www.linkedin.com/in/ruben-arevalo-4944251b7/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
                     <li><a href="https://github.com/rchrisarevalo" target="_blank" rel="noopener noreferrer">Github</a></li>
                 </div>
