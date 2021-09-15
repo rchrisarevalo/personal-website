457e87797b16e3db2227a3844bc400aae10914bb
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-09-14 20:41:55 -0500

    Formatted document.

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -12,11 +12,11 @@ const Load = (props) => {
     }, 3000)
 
     return (
-        <div className = "loading-screen">
-            <div className = "loading-animation">
-                <Spinner animation="grow" variant="white" id="delay-1"/>
-                <Spinner animation="grow" variant="white" id="delay-2"/>
-                <Spinner animation="grow" variant="white" id="delay-3"/>
+        <div className="loading-screen">
+            <div className="loading-animation">
+                <Spinner animation="grow" variant="white" id="delay-1" />
+                <Spinner animation="grow" variant="white" id="delay-2" />
+                <Spinner animation="grow" variant="white" id="delay-3" />
             </div>
         </div>
     )
