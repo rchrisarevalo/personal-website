caefba5ac251b0eaf399f399f03104fc924a38ba
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-09-14 20:42:22 -0500

    Formatted document.

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Error404 = () => {
     return (
-        <div className = "error-container">
-            <div className = "error-message">
+        <div className="error-container">
+            <div className="error-message">
                 <h1>ERROR 404!</h1>
                 <p>Oops! It looks like what you were looking for does not exist on this website!</p>
                 <p>Here are some links below that can help you resolve this issue:</p>
