1bcf865e8976217bd9b4c0776fb8685fb8a13811
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-09-14 20:41:41 -0500

    Formatted document.

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Notification = () => {
     return (
         <div className="notification-container">
-            <div className = "notification" id="warning-notif">
-                <div className = "notification-content" id="notif-1">
+            <div className="notification" id="warning-notif">
+                <div className="notification-content" id="notif-1">
                     <p>This is an image I took. Please don't copy it without my permission. :)</p>
                 </div>
             </div>
-            <div className = "notification" id="warning-notif-2">
-                <div className = "notification-content" id="background-green">
+            <div className="notification" id="warning-notif-2">
+                <div className="notification-content" id="background-green">
                     <p>Redirecting to contact form page...</p>
                 </div>
             </div>
