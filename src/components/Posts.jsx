7e8014ea83d991f5532266cd13d4d6de49463bfa
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-09-18 00:22:25 -0500

    UI fix in mobile verison.

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -45,7 +45,7 @@ const Posts = () => {
                     Old posts will be <b>archived</b> on the last day of each month. Any posts that have not reached
                     this deadline yet will stay on this page.
                 </p>
-                <div className="post">
+                <div className="post" id="post-intro-margin">
                     <p id="post-content">
                         This will be brief. I updated the website's font to make it more appealing for users visiting
                         this site. I hope you enjoy this update!
@@ -57,7 +57,7 @@ const Posts = () => {
                         Posted on 9/15/2021 at 1:12 AM.
                     </p>
                 </div>
-                <div className="post">
+                <div className="post" id="post-intro-margin">
                     <p id="post-content">
                         I am leaving this here to let y'all know that I am doing fine! I am really busy doing
                         college work and I am currently bombarded by a lot of homework. I am trying my best to
@@ -70,7 +70,7 @@ const Posts = () => {
                         Posted on 9/14/2021 at 8:39 PM.
                     </p>
                 </div>
-                <div className="post">
+                <div className="post" id="post-intro-margin">
                     <p id="post-content">
                         Happy Labor Day, everyone! I hope y'all have a wonderful one!
                     </p>
@@ -81,7 +81,7 @@ const Posts = () => {
                         Posted on 9/6/2021 at 1:19 AM.
                     </p>
                 </div>
-                <div className="post">
+                <div className="post" id="post-intro-margin">
                     <p id="post-content">
                         I want to make another major announcement regarding my post archival policy. I have decided to
                         archive all posts <b>by the last day of each month</b>. In other words, I will completely wipe off
