dd87118d92587b5cac435a6642041b141db0bf1e
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-09-18 01:03:20 -0500

    New post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -47,6 +47,29 @@ const Posts = () => {
                 </p>
                 <div className="post" id="post-intro-margin">
                     <p id="post-content">
+                        Hello everyone! I am here to announce yet another UI change, only this time, the posts appear as if they are glowing.
+                        I added a little bit of touch to the UI of the posts to make it more modern and appealing. There have also been
+                        some changes to the mobile app as well, such as centering the "About the Author" section in the "About Me"
+                        page since a left text align did not suit well in the mobile version.
+                    </p>
+                    <p id="post-content">
+                        If you have any issues regarding the user interface or the overall function of this page, please do not hesitate to
+                        contact me through either the "Report" or "Contact Me" form by clicking one of the buttons on the top right of
+                        the page (or the top right for the former or the "Report" option that can be found in the navigation bar in the
+                        mobile version).
+                    </p>
+                    <p id="post-content">
+                        Have a wonderful rest of the day (if you happen to be awake to see this, yes I stay up late most of the time).
+                    </p>
+                    <p id="post-content">
+                        -- Ruben Christopher Arevalo
+                    </p>
+                    <p id="post-date">
+                        Posted on 9/18/2021 at 1:04 AM.
+                    </p>
+                </div>
+                <div className="post" id="post-intro-margin">
+                    <p id="post-content">
                         This will be brief. I updated the website's font to make it more appealing for users visiting
                         this site. I hope you enjoy this update!
                     </p>
