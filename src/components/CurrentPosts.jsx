bb4c8ba391e1cbda994d4fe441a524e85a779f32
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-09-30 01:05:46 -0500

    New posts.

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -34,10 +34,37 @@ const CurrentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-content">
-                   Hello everyone. I am leaving this here to let y'all know that I had to remove the description boxes
-                   for each of my works in the "About Me" page. I had to do this because I noticed huge styling errors
-                   that made it appear worse in other devices. Thankfully, I managed to fix this issue, and even though
-                   I am sad to remove the descriptions, it was for the best. I apologize for the inconvenience.
+                    In addition, this will be the last post for this month. The whole "Announcements"
+                    section (with the exception of 'Recent Announcements') will be wiped and replaced
+                    with a post (or posts) for the new month (which is October).
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 9/30/2021 at 1:06 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    Hello everyone. I am happy to announce a new UI design for the footer in this page.
+                    New information has been included in the copyright notice. This new information
+                    should deter any attempts of copyright infringement from people intending to
+                    copy this site without permission.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 9/30/2021 at 1:06 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    Hello everyone. I am leaving this here to let y'all know that I had to remove the description boxes
+                    for each of my works in the "About Me" page. I had to do this because I noticed huge styling errors
+                    that made it appear worse in other devices. Thankfully, I managed to fix this issue, and even though
+                    I am sad to remove the descriptions, it was for the best. I apologize for the inconvenience.
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,6 +10,33 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-intro-margin">
                 <p id="post-content">
+                   In addition, this will be the last post for this month. The whole "Announcements"
+                   section (with the exception of 'Recent Announcements') will be wiped and replaced
+                   with a post (or posts) for the new month (which is October).
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 9/30/2021 at 1:06 AM.
+                </p>
+            </div>
+            <div className="post" id="post-intro-margin">
+                <p id="post-content">
+                   Hello everyone. I am happy to announce a new UI design for the footer in this page.
+                   New information has been included in the copyright notice. This new information
+                   should deter any attempts of copyright infringement from people intending to
+                   copy this site without permission.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 9/30/2021 at 1:06 AM.
+                </p>
+            </div>
+            <div className="post" id="post-intro-margin">
+                <p id="post-content">
                    Hello everyone. I am leaving this here to let y'all know that I had to remove the description boxes
                    for each of my works in the "About Me" page. I had to do this because I noticed huge styling errors
                    that made it appear worse in other devices. Thankfully, I managed to fix this issue, and even though
@@ -20,32 +47,6 @@ const RecentPosts = () => {
                 </p>
                 <p id="post-date">
                     Posted on 9/28/2021 at 1:12 AM.
-                </p>
-            </div>
-            <div className="post" id="post-intro-margin">
-                <p id="post-content">
-                    Hi everyone! I am leaving this announcement here to let y'all know that this website underwent a UI
-                    (user interface) design change once again. I changed the color of the navigation bar, the posts, and
-                    redesigned the "Works" section in the "About Me" page. I hope you all enjoy the new design!
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 9/26/2021 at 3:43 PM.
-                </p>
-            </div>
-            <div className="post" id="post-intro-margin">
-                <p id="post-content">
-                    In addition, I reduced the announcement margins for this page only. The ones in the "Recent Announcements"
-                    section in the "About Me" page are not affected by this change. Once again, I made this change so that
-                    it can be similar to the posts in the "About Me" page.
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 9/25/2021 at 11:25 AM.
                 </p>
             </div>
         </div>
