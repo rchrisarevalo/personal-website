25bfb73b0e8a3d4620a61a1c65d93033301be9bd
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-10-15 09:52:10 -0500

    New posts.

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -34,6 +34,60 @@ const CurrentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-content">
+                    I will continue to post announcements here occasionally to let y'all know how I am doing with
+                    my life so far in case this website has not been updated in the past 7 days or so. So thank you
+                    everyone, and have a great and safe weekend!
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 10/15/2021 at 9:54 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    Also, I am not going to be updating this website as frequently as I did in the past three months.
+                    Since I am a college student, I am a critical point of the semester where I must be focusing on
+                    passing my courses so as to ensure my continued progress on attaining my bachelor's degree.
+                    I hope that I make enough time to work on this website and most likely restyle it for this upcoming
+                    Halloween on October 31st.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 10/15/2021 at 9:54 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    So if you want to contact me at any given moment for any issue you have or to leave a comment, please
+                    feel free to press the "Contact Me" button on the top right of the navigation bar (or the message icon
+                    on the top right on the mobile version of this site.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 10/15/2021 at 9:54 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    Hi everyone! It is with deep regret that I have decided to remove the 'Report' button from
+                    the website. The reason for this decision was because the "Report" form was similar to the
+                    to the "Contact Me" form, since they both involve a degree of contact.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 10/15/2021 at 9:54 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
                     A new profile pic has been added (again). I hope you all like it!
                 </p>
                 <p id="post-content">
