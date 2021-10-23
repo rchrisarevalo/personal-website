4f9bad669872ce27bae6bc6b65a499c387508ee7
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-10-23 15:02:20 -0500

    New posts added to 'Recent Post' section

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -8,6 +8,35 @@ const RecentPosts = () => {
                 The last three recent posts will be posted here. This page will be updated daily.
                 To view all posts, click on "Announcements" on the navigation bar.
             </p>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    If you have any questions, please feel free to contact me through the form or my email
+                    if you have any suggestions or comments about the design!
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 10/23/2021 at 3:06 PM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    Hi everyone! I hope y'all are having a great weekend! I am happy to announce that I have
+                    redesigned this website to fit the theme of this year's upcoming Halloween festivites.
+                    This design will stay up until November 1st, at 12 AM. After that deadline has passed,
+                    then the website will revert to its original design.
+                </p>
+                <p id="post-content">
+                    Thank you and have a great weekend!
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 10/23/2021 at 3:06 PM.
+                </p>
+            </div>
             <div className="post" id="post-intro-margin">
                 <p id="post-content">
                     I will also be changing how this website operates, especially during the holidays. I will
@@ -22,37 +51,6 @@ const RecentPosts = () => {
                 </p>
                 <p id="post-date">
                     Posted on 10/22/2021 at 6:07 PM.
-                </p>
-            </div>
-            <div className="post" id="post-intro-margin">
-                <p id="post-content">
-                    Hi everyone! I am just leaving this here to wish y'all a good weekend! I had a very
-                    busy week, working on assignments and quizzes assigned to me as usual. I want to
-                    let you all know that I will start working on the Halloween decor for this website
-                    as part of Halloween.
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 10/22/2021 at 6:07 PM.
-                </p>
-            </div>
-            <div className="post" id="post-intro-margin">
-                <p id="post-content">
-                    In addition, I will be updating this page for this upcoming Halloween at the
-                    end of the month. The page will temporarily have the colors of Halloween,
-                    such as orange, black, purple, etc. While it may not look pretty in the end,
-                    I will try my best to make it look good.
-                </p>
-                <p id="post-content">
-                    Have a great day, everyone!
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 10/18/2021 at 12:06 PM.
                 </p>
             </div>
         </div>
