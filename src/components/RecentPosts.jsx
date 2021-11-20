aa18915c0d317fa7956ba1a0588145dd998d3158
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-11-20 10:41:11 -0600

    New recent post 1.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -8,7 +8,19 @@ const RecentPosts = () => {
                 The last three recent posts will be posted here. This page will be updated daily.
                 To view all posts, click on "Announcements" on the navigation bar.
             </p>
-            
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    Starting this Thanksgiving and any other holiday from that point on, the website will have a 
+                    fixed size screen that will tell visitors that operations have ceased for a day due to the holidays
+                    and that they will resume as normal after they are over.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 11/20/2021 at 11:00 AM.
+                </p>
+            </div>
         </div>
     );
 }
