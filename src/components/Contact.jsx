4bfc53e564f1c79e83da2879a13981b2fa28b707
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-11-20 10:47:36 -0600

    Contact page info updated.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,17 +22,14 @@ const Contact = () => (
             <p>
                 You can now contact me by clicking the "Contact Me" button the navigation bar on the web version of
                 this site. On the mobile version, you can click on the paper airplane icon to access the contact form
-                page to leave a message or contact me.
+                page to leave a message.
             </p>
             <p>
                 If you have any questions, please feel free to contact me through my email, <a href="mailto:rubenplayer105@gmail.com" id="contact-link"><b>rubenplayer105@gmail.com</b></a>,
                 LinkedIn, or Github. The links for both my LinkedIn and Github profiles are located on the footer below.
-                If you have any complaints or issues with this website, please feel free to click on the
-                report button on the web version. On the mobile version, click on the three bars on the
-                top left of the header to access the navigation menu, and click on the report button.
             </p>
             <p>
-                I will most likely respond to complaints/reports in about 3-5 days. If I am super available
+                I will most likely respond to sent messages in about 3-5 days. If I am super available
                 (meaning if I literally have nothing to do), then I might respond to your issue much earlier
                 than anticipated response time provided above.
             </p>
