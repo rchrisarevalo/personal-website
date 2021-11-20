e0580f38f49aa14359c5c3a689e145e362e3f4be
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-11-20 10:42:33 -0600

    New recent post 3.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -8,6 +8,19 @@ const RecentPosts = () => {
                 The last three recent posts will be posted here. This page will be updated daily.
                 To view all posts, click on "Announcements" on the navigation bar.
             </p>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    Despite this, I will leave a "Contact" button where everyone is able to leave me a message that I
+                    can respond to after the holidays are over. I hope you all have a fantastic Thanksgiving!
+                    Please stay safe and enjoy!
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 11/20/2021 at 11:00 AM.
+                </p>
+            </div>
             <div className="post" id="post-intro-margin">
                 <p id="post-content">
                     In addition, the website navigation bar for each page will be removed so as to indicate my point
