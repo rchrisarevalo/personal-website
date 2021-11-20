3ba8a6264698f9621cdcb35e0fdefb294a08b029
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-11-20 10:40:35 -0600

    Deleted recent posts to replace with new posts.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -8,44 +8,7 @@ const RecentPosts = () => {
                 The last three recent posts will be posted here. This page will be updated daily.
                 To view all posts, click on "Announcements" on the navigation bar.
             </p>
-            <div className="post" id="post-intro-margin">
-                <p id="post-content">
-                    Now that I am done delivering these announcements, I wish you all a good day. Please
-                    be happy and stay safe!
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 11/10/2021 at 8:55 AM.
-                </p>
-            </div>
-            <div className="post" id="post-intro-margin">
-                <p id="post-content">
-                    Also, next week, I will announce some big changes as to how this website is going to
-                    operate (well, one big change).
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 11/10/2021 at 8:55 AM.
-                </p>
-            </div>
-            <div className="post" id="post-intro-margin">
-                <p id="post-content">
-                    However, that doesn't take away my time to announce that I will put up a Thanksgiving
-                    decoration makeup here on my website. During the week of Thanksgiving, I am going to
-                    be working on applying the decoration the week before the holidays, and making sure
-                    it is enjoyable for people visiting this website.
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 11/10/2021 at 8:55 AM.
-                </p>
-            </div>
+            
         </div>
     );
 }
