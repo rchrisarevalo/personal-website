44dbc834651cc93f0fd7a39983e9e3f4ac468715
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-11-21 15:57:49 -0600

    Recent posts section updated with new post.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -8,6 +8,19 @@ const RecentPosts = () => {
                 The last three recent posts will be posted here. This page will be updated daily.
                 To view all posts, click on "Announcements" on the navigation bar.
             </p>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    I already implemented the Thanksgiving decoration for this website, just as I promised.
+                    The decoration just contains only the colors of the holiday to fit in with the Thanksgiving
+                    theme. Have a wonderful holiday and stay safe!
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 11/21/2021 at 4:01 PM.
+                </p>
+            </div>
             <div className="post" id="post-intro-margin">
                 <p id="post-content">
                     Despite this, I will leave a "Contact" button where everyone is able to leave me a message that I
@@ -25,19 +38,6 @@ const RecentPosts = () => {
                 <p id="post-content">
                     In addition, the website navigation bar for each page will be removed so as to indicate my point
                     that the website is closed for the holidays.
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 11/20/2021 at 11:00 AM.
-                </p>
-            </div>
-            <div className="post" id="post-intro-margin">
-                <p id="post-content">
-                    Starting this Thanksgiving and any other holiday from that point on, the website will have a 
-                    fixed size screen that will tell visitors that operations have ceased for a day due to the holidays
-                    and that they will resume as normal after they are over.
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
