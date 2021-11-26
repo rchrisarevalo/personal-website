35536ac3b6e2f8ee7f150f7f2743a95ed51c223f
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-11-25 20:21:35 -0600

    Additional comments.

--- src/components/ClosedWeb.jsx
+++ src/components/ClosedWeb.jsx
@@ -6,9 +6,14 @@ const ClosedWeb = () => {
         <div className="error-container">
             <div className="error-message">
                 <h1>Happy Thanksgiving, everyone!</h1>
-                <p>As part of a new website operating policy, I have deciced to close this website</p>
-                <p>for today due to the holidays. If you have any questions, please feel free to</p>
-                <p>contact me by clicking the "Contact Me" button below the description.</p>
+                <p>
+                    As part of a new website operating policy, I have deciced to close this website
+                    for today due to the holidays. If you have any questions, please feel free to
+                    contact me by clicking the "Contact Me" button below the description.
+                </p>
+                <p>
+                    This website will re-open on November 26th, 12:00 AM.
+                </p>
                 <br />
                 <p>Have a wonderful holiday and stay safe!</p>
                 <br/>
