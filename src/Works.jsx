85c9c5a7559b27aecd14aa23f0e504d74e9fbc6b
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-06 11:34:46 -0600

    h3 tags removed from works section.

--- src/Works.jsx
+++ src/Works.jsx
@@ -16,27 +16,21 @@ const Works = () => {
                 skills, and I am going to try to improve as much as I can.
             </p>
             <div className="works-1">
-                <h3><i>Curriculum Vitae (CV) Website</i></h3>
                 <img src={thumb1} alt="work-1"></img>
             </div>
             <div className="works-2">
-                <h3><i>Look Video and Photography Website</i></h3>
                 <img src={thumb2} alt="work-2"></img>
             </div>
             <div className="works-3">
-                <h3><i>A Sky and Palm Background</i></h3>
                 <img src={skyshot1} alt="work-3"></img>
             </div>
             <div className="works-4">
-                <h3><i>Clear Sky with a Palm Background (November 2021)</i></h3>
                 <img src={skyshot2} alt="work-4"></img>
             </div>
             <div className="works-5">
-                <h3><i>Clear Sky with a Palm Background (March 2021)</i></h3>
                 <img src={skyshot3} alt="work-5"></img>
             </div>
             <div className="works-6">
-                <h3><i>Clear Sky with a Palm Background (AGAIN) (July 2021)</i></h3>
                 <img src={skyshot4} alt="work-6"></img>
             </div>
         </div>
