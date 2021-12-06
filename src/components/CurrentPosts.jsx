7132f65f0c7c3247dfeb2e9deb01879bc64e32fc
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-06 11:41:15 -0600

    New post.

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -36,6 +36,19 @@ const CurrentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-content">
+                    As you all may have noticed, another font change has occurred. I did this because I thought
+                    that the old font used for the previous 2-3 months was boring, and I thought it was time for
+                    a change. But it looks good, and hopefully y'all enjoy it as well!
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 12/06/2021 at 11:43 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
                     Anyways, that's all I have to say at the moment. Again, if you have any questions or suggestions
                     as to how to update this site, please let me know by clicking on the "Contact Me" button on the
                     top right corner of the page (or the paper plane icon in the mobile version of this site).
