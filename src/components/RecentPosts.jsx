638dfc79548fef0a7ff2258b4cc463a0165aff4f
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-06 11:41:50 -0600

    New recent post included.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,6 +10,19 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-content">
+                    As you all may have noticed, another font change has occurred. I did this because I thought
+                    that the old font used for the previous 2-3 months was boring, and I thought it was time for
+                    a change. But it looks good, and hopefully y'all enjoy it as well!
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 12/06/2021 at 11:43 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
                     Anyways, that's all I have to say at the moment. Again, if you have any questions or suggestions
                     as to how to update this site, please let me know by clicking on the "Contact Me" button on the
                     top right corner of the page (or the paper plane icon in the mobile version of this site).
@@ -29,20 +42,6 @@ const RecentPosts = () => {
                     After that's all said and done, I will focus on providing a new user interface for this website
                     sometime during January or February, if time permits, as I will be back in school for another
                     semester during that time.
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 12/03/2021 at 9:40 PM.
-                </p>
-            </div>
-            <div className="post" id="post-margin">
-                <p id="post-content">
-                    Hi everyone! Sorry for the late changes, but I was busy with college as finals are already
-                    approaching. In addition, I might not post as often as I do this month because I have decided
-                    to take a 2-week hiatus from updating this site starting on December 13 and ending on December 27,
-                    right after Christmas.
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
