5b17d74c29db8d761280cbfbd94d2b21250b28c3
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-14 00:44:32 -0600

    New posts.

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -36,6 +36,46 @@ const CurrentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-content">
+                    I will try to read all of your messages after my 2 week hiatus is up. The same will apply
+                    after New Year's Eve and New Year's. Have a wonderful day and Merry Christmas!
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 12/14/2021 at 12:54 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    I will be working on the website and design a user interface as we approach the next new year
+                    (2022). So if I don't post any announcements for the majority of this month, it's because I
+                    am on break in addition to making updates to this website. I sincerely apologize for any
+                    inconvenience that might occur along the way, such as not responding to messages from y'all
+                    in a timely manner.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 12/14/2021 at 12:54 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
+                    Good evening, everyone. I know I am on a hiatus right now for the next two weeks, but I wanted
+                    to leave this message here for anyone visiting this website to let y'all know that I am not
+                    going to be posting any content here during the break.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 12/14/2021 at 12:54 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
                     As you all may have noticed, another font change has occurred. I did this because I thought
                     that the old font used for the previous 2-3 months was boring, and I thought it was time for
                     a change. But it looks good, and hopefully y'all enjoy it as well!
