f229fc6342460b1fc1eaf3f28fd442c4ff382525
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-14 01:13:57 -0600

    Update message added.

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BsExclamationCircle } from "react-icons/bs";
-import { IoCloseCircleOutline } from "react-icons/io5";
+import { IoCloseCircleOutline, IoAlertCircleOutline } from "react-icons/io5";
 
 function closeUpdateMsg() {
     var e = document.getElementById("close-msg")
@@ -14,10 +13,23 @@ const Update = () => {
         <div className="update-message" id="close-msg">
             <IoCloseCircleOutline onClick={closeUpdateMsg} id="close-icon" />
             <p id="update-title">
-                <BsExclamationCircle size="18px" id="update-icon" /> UPDATE:
+                <IoAlertCircleOutline size="23px" id="update-icon" /> UPDATE:
             </p>
             <p>
-                This is a test update using a separate component that will make it easier to modify this page. It works!
+                Hi everyone! I am leaving this message here to let everyone know that I will be in a 
+                <b> 2 week hiatus</b> that started yesterday on December 13. I will not be posting anything
+                here for the above mentioned duration as not only will I be spending my break with my family
+                and friends after a difficult college semester, but I will also be working on updating this
+                website as well. I will be designing a new user interface that will be released after New Year's
+                is over. For more details as to how my hiatus and the holidays will affect the way how I operate
+                this website, please feel free to look at the <b>"Recent Announcements"</b> section of this page, or click
+                on the <b>"Announcements"</b> link to view the posts regarding this topic.
+            </p>
+            <p>
+                Thank you everyone! Stay safe and happy holidays!
+            </p>
+            <p>
+                <i>-- Ruben Christopher Arevalo</i>
             </p>
             <br></br>
         </div>
