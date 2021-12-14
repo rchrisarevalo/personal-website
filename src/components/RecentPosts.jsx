b38a406ee5302e6cc4d9fc08405c0ed7cd31ac1b
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-14 00:46:22 -0600

    Recent posts section updated.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,44 +10,42 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-content">
-                    As you all may have noticed, another font change has occurred. I did this because I thought
-                    that the old font used for the previous 2-3 months was boring, and I thought it was time for
-                    a change. But it looks good, and hopefully y'all enjoy it as well!
+                    I will try to read all of your messages after my 2 week hiatus is up. The same will apply
+                    after New Year's Eve and New Year's. Have a wonderful day and Merry Christmas!
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
                 </p>
                 <p id="post-date">
-                    Posted on 12/06/2021 at 11:43 AM.
+                    Posted on 12/14/2021 at 12:54 AM.
                 </p>
             </div>
             <div className="post" id="post-margin">
                 <p id="post-content">
-                    Anyways, that's all I have to say at the moment. Again, if you have any questions or suggestions
-                    as to how to update this site, please let me know by clicking on the "Contact Me" button on the
-                    top right corner of the page (or the paper plane icon in the mobile version of this site).
-                </p>
-                <p id="post-content">
-                    Thank you and have a great rest of the evening!
+                    I will be working on the website and design a user interface as we approach the next new year
+                    (2022). So if I don't post any announcements for the majority of this month, it's because I
+                    am on break in addition to making updates to this website. I sincerely apologize for any
+                    inconvenience that might occur along the way, such as not responding to messages from y'all
+                    in a timely manner.
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
                 </p>
                 <p id="post-date">
-                    Posted on 12/03/2021 at 9:40 PM.
+                    Posted on 12/14/2021 at 12:54 AM.
                 </p>
             </div>
             <div className="post" id="post-margin">
                 <p id="post-content">
-                    After that's all said and done, I will focus on providing a new user interface for this website
-                    sometime during January or February, if time permits, as I will be back in school for another
-                    semester during that time.
+                    Good evening, everyone. I know I am on a hiatus right now for the next two weeks, but I wanted
+                    to leave this message here for anyone visiting this website to let y'all know that I am not
+                    going to be posting any content here during the holidays, especially right now.
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
                 </p>
                 <p id="post-date">
-                    Posted on 12/03/2021 at 9:40 PM.
+                    Posted on 12/14/2021 at 12:54 AM.
                 </p>
             </div>
         </div>
