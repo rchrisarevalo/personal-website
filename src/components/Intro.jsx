1cca1f01f8bd4df8d307276d612c22f4868d1209
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-14 01:15:46 -0600

    "About the Author" section updated.

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -24,6 +24,8 @@ const Intro = () => {
                 <p>
                     Hi everyone! My name is Ruben Christopher Arevalo, and I am a third-year student attending the University of Texas-Rio Grande Valley.
                     I am currently pursuing my bachelor's degree in computer engineering with my concentration focusing on software.
+                    Fun facts I want to share about myself are that I love to code, listen to music (preferrably lofi, classical, and pop),
+                    play video games, and watch movies and shows in my free time.
                     Since I was a high school student, I always found the concept of programming intriguing, thinking about how
                     much several lines of code can change the world in so many ways. This led to me to develop a huge interest in
                     learning about software development, since there are so many software programs out there in the Internet,
