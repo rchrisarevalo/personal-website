09d5a9d723ed4746c96a8f5f82dba96de722cec6
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-19 11:48:30 -0600

    Closed website information updated.

--- src/components/ClosedWeb.jsx
+++ src/components/ClosedWeb.jsx
@@ -5,17 +5,10 @@ const ClosedWeb = () => {
 
         <div className="error-container">
             <div className="error-message">
-                <h1>Merry Christmas, everyone!</h1>
                 <p id="closed-msg">
                     In accordance to the new website operation policy I have implemented in the last month,
-                    I decided to close this website today on December 19, 2021. Since the holiday week starts
-                    tomorrow, I have decided that it was the best idea to close this website and open it up
-                    again right after Christmas ends. However, the possibility of closing this website until
-                    the end of New Year's is not ruled out yet. I will update this page on Christmas Eve when I
-                    make my final decision.
-                </p>
-                <p>
-                    <b>For now, this website will re-open on December 26th, at 12 AM. Keep in mind that this opening time is subject to change.</b>
+                    I decided to close this website today on December 19, 2021. This website will most likely
+                    re-open on <b>December 26th at 12 AM</b> (though it may be extended until after New Year's is over).
                 </p>
                 <br />
                 <p id="closed-msg">Have a wonderful Merry Christmas and stay safe!</p>
