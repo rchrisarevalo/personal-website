9daa12d31ccf52e2da8a35d636af47460979956f
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-19 11:37:35 -0600

    Closed page edited.

--- src/components/ClosedWeb.jsx
+++ src/components/ClosedWeb.jsx
@@ -5,20 +5,23 @@ const ClosedWeb = () => {
 
         <div className="error-container">
             <div className="error-message">
-                <h1>Happy Thanksgiving, everyone!</h1>
-                <p>
-                    As part of a new website operating policy, I have deciced to close this website
-                    for today due to the holidays. If you have any questions, please feel free to
-                    contact me by clicking the "Contact Me" button below the description.
+                <h1>Merry Christmas, everyone!</h1>
+                <p id="closed-msg">
+                    In accordance to the new website operation policy I have implemented in the last month,
+                    I decided to close this website today on December 19, 2021. Since the holiday week starts
+                    tomorrow, I have decided that it was the best idea to close this website and open it up
+                    again right after Christmas ends. However, the possibility of closing this website until
+                    the end of New Year's is also in the air. I will update this page on Christmas Eve when I
+                    make my final decision.
                 </p>
                 <p>
-                    This website will re-open on November 26th, 12:00 AM.
+                    <b>For now, this website will re-open on December 26th, at 12 AM. Keep in mind that this opening time is subject to change.</b>
                 </p>
                 <br />
-                <p>Have a wonderful holiday and stay safe!</p>
+                <p id="closed-msg">Have a wonderful Merry Christmas and stay safe!</p>
                 <br/>
                 <div id="closed-button">
-                    <a href="youtube.com">Contact Me</a>
+                    <a href="https://youtube.com" rel="noreferrer" target="_blank">Contact Me</a>
                 </div>
                 <br></br>
             </div>
