f05fc86c883d21e3cd5d05c8f30dc93b647a9a80
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-20 01:29:08 -0600

    Syntax error correction.

--- src/components/ClosedWeb.jsx
+++ src/components/ClosedWeb.jsx
@@ -8,7 +8,7 @@ function currentDate()
 
     var stringMonth = " ";
 
-    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     for (var i = 0; i < months.length; i++)
     {
