0ab38a69b7cd0f24c6110d556fdcfdad7fcc65f4
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-20 11:42:22 -0600

    Routes reconfigured.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import React from 'react';
 import Load from "./components/Load.jsx";
 import Intro from "./components/Intro.jsx";
 import Posts from "./components/Posts.jsx";
-import August_Posts from "./components/August_Posts.jsx";
-import September_Posts from "./components/September_Posts.jsx";
-import October_Posts from "./components/October_Posts.jsx";
-import November_Posts from "./components/November_Posts.jsx";
+import August2021Posts from "./components/2021 Posts Components/August2021Posts.jsx";
+import September2021Posts from "./components/2021 Posts Components/September2021Posts.jsx";
+import October2021Posts from "./components/2021 Posts Components/October2021Posts.jsx";
+import November2021Posts from "./components/2021 Posts Components/November2021Posts.jsx";
 import Archives from "./components/Archives.jsx";
 import Error404 from "./components/Error404.jsx";
 
@@ -21,10 +21,10 @@ const App = () => {
         <Route path="/" exact component={Load}/>
         <Route path ="/about" exact component={Intro}/>
         <Route path="/announcements" exact component={Posts}/>
-        <Route path="/announcements/2021/August" exact component={August_Posts}/>
-        <Route path="/announcements/2021/September" exact component={September_Posts}/>
-        <Route path="/announcements/2021/October" exact component={October_Posts}/>
-        <Route path="/announcements/2021/November" exact component={November_Posts}/>
+        <Route path="/announcements/2021/August" exact component={August2021Posts}/>
+        <Route path="/announcements/2021/September" exact component={September2021Posts}/>
+        <Route path="/announcements/2021/October" exact component={October2021Posts}/>
+        <Route path="/announcements/2021/November" exact component={November2021Posts}/>
         <Route path="/archives" exact component={Archives}/>
         <Route exact component={Error404}/>
       </Switch>
