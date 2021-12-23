10a3f7a5a0d4537ccd531dec38cdd50ceab7c568
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-23 11:02:25 -0600

    Routes reconfigured. Will be deployed on Jan 2.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import React from 'react';
 
 // Components that will be used as page routes
 import Load from "./components/Load.jsx";
+import Intro from "./components/Intro.jsx";
+import Posts from "./components/Posts.jsx";
+import August2021Posts from "./components/2021 Posts Components/August2021Posts.jsx";
+import September2021Posts from "./components/2021 Posts Components/September2021Posts.jsx";
+import October2021Posts from "./components/2021 Posts Components/October2021Posts.jsx";
+import November2021Posts from "./components/2021 Posts Components/November2021Posts.jsx";
+import December2021Posts from './components/2021 Posts Components/December2021Posts';
+import Archives from "./components/Archives.jsx";
+import WebsiteOperation from "./components/policies/WebsiteOperation.jsx"
 import Error404 from "./components/Error404.jsx";
 import ClosedWeb from "./components/ClosedWeb.jsx";
 
@@ -14,6 +23,15 @@ const App = () => {
     <div className="App">
       <Switch>
         <Route path="/" exact component={Load}/>
+        <Route path ="/about" exact component={Intro}/>
+        <Route path="/announcements" exact component={Posts}/>
+        <Route path="/announcements/2021/August" exact component={August2021Posts}/>
+        <Route path="/announcements/2021/September" exact component={September2021Posts}/>
+        <Route path="/announcements/2021/October" exact component={October2021Posts}/>
+        <Route path="/announcements/2021/November" exact component={November2021Posts}/>
+        <Route path="/announcements/2021/December" exact component={December2021Posts}/>
+        <Route path="/archives" exact component={Archives}/>
+        <Route path="/site-operation" exact component={WebsiteOperation}/>
         <Route path="/closed" exact component={ClosedWeb}/>
         <Route exact component={Error404}/>
       </Switch>
