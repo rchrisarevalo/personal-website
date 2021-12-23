800ee3d5bc7cb372f946e78716e72361addb9a1a
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-23 10:54:31 -0600

    Days left added and message updated.

--- src/components/ClosedWeb.jsx
+++ src/components/ClosedWeb.jsx
@@ -19,24 +19,35 @@ function currentDate()
     return [stringMonth, " ", day, ", ", year];
 }
 
+function daysLeft()
+{
+    const current_date = new Date().getDate();
+    const set_date = new Date().getDate() + 11;
+
+    var future_date = Math.abs(current_date - set_date);
+
+    return future_date;
+}
+
 const ClosedWeb = () => {
     return (
 
         <div className="center-container">
             <div className="center-message">
                 <p id="closed-msg">Today's date: <i>{currentDate()}</i></p>
+                <p id="closed-msg">Days left: <i>{daysLeft()}</i></p>
                 <p id="closed-msg">
                     In accordance to the new website operation policy I have implemented in the last month,
-                    I decided to close this website today on December 19, 2021. This website will most likely
-                    re-open on <b>December 26th at 12 AM</b> (though it may be extended until after New Year's is over).
+                    I decided to close this website today on December 19, 2021. I have decided to push the
+                    re-opening date of this website to January 2nd, 2022 in observance of Christmas and 
+                    New Year's.
                 </p>
                 <br />
-                <p id="closed-msg">Have a wonderful Merry Christmas and stay safe!</p>
+                <p id="closed-msg">Have a wonderful holiday and stay safe!</p>
                 <br/>
                 <div id="closed-button">
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSe1nR8hmOAMRTU3fDZsbLRGkjzKvA9uRLZW_YdvsyBEctqDOw/viewform" rel="noreferrer" target="_blank">Contact Me</a>
                 </div>
-                <br></br>
             </div>
         </div>
     )
