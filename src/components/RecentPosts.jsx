515683e9cf057a93bd09836bb4059c9e0a393f51
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2021-12-26 18:49:38 -0600

    Last three recent posts for this month added.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,42 +10,57 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-content">
-                    I will try to read all of your messages after my 2 week hiatus is up. The same will apply
-                    after New Year's Eve and New Year's. Have a wonderful day and Merry Christmas!
+                    That's all for now. Have a great rest of the week and please stay safe!
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
                 </p>
                 <p id="post-date">
-                    Posted on 12/14/2021 at 12:54 AM.
+                    Posted on 01/03/2022 at 12:00 AM.
                 </p>
             </div>
             <div className="post" id="post-margin">
                 <p id="post-content">
-                    I will be working on the website and design a user interface as we approach the next new year
-                    (2022). So if I don't post any announcements for the majority of this month, it's because I
-                    am on break in addition to making updates to this website. I sincerely apologize for any
-                    inconvenience that might occur along the way, such as not responding to messages from y'all
-                    in a timely manner.
+                    Now that I am done covering the new "Website Operation" policy, I also wanted to let y'all know
+                    that I am back and will be maintaining this site as before. However, expect my activity to dwindle
+                    down a little bit on January 18th and afterwards, as the Spring semester of my university starts
+                    that day. Despite that, I will continue to make an effort to maintain this Site and keep it running
+                    without any bugs or any regressions that might be introduced in the future.
+                </p>
+                <p id="post-content">
+                    Since I am speaking about bugs right now, I will also keep an eye out on issues that might be caused
+                    by a vulnerability with Log4j, which is widely used in many pieces of software around the world, such
+                    as our smart phones, computers, video games, etc. As this website is running on GitHub Pages, I am
+                    going to do my best to keep an eye out for any issues that might arise in the future as a result of
+                    this vulnerability.
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
                 </p>
                 <p id="post-date">
-                    Posted on 12/14/2021 at 12:54 AM.
+                    Posted on 01/03/2022 at 12:00 AM.
                 </p>
             </div>
             <div className="post" id="post-margin">
                 <p id="post-content">
-                    Good evening, everyone. I know I am on a hiatus right now for the next two weeks, but I wanted
-                    to leave this message here for anyone visiting this website to let y'all know that I am not
-                    going to be posting any content here during the holidays, especially right now.
+                    In addition, I wanted to remind you all that I introduced a new policy that determines what
+                    time this Site can and cannot operate. The new policy was introduced in November 2021, just
+                    before I closed this website for the Thanksgiving holiday. However, I forgot to specifically
+                    outline when and what times the website can operate.
+                </p>
+                <p id="post-content">
+                    Fortunately, I drafted an outline as to how this Site will operate from now on. Keep in mind
+                    that the new policy that is titled "Website Operation Policy" is not a legal document per se,
+                    but rather, to explain what is going to happen to the operations of this Site. The page containing
+                    that policy will explain what circumstances would warrant the closure of this Site, along with
+                    other additional details. For more information, please feel free to click on the "Website Operation"
+                    link on the footer in the "Policies" column.
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
                 </p>
                 <p id="post-date">
-                    Posted on 12/14/2021 at 12:54 AM.
+                    Posted on 01/03/2022 at 12:00 AM.
                 </p>
             </div>
         </div>
