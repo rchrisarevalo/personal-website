dca54e7be60e39d521bfd73e639ad818abb30c3b
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-01-01 00:23:53 -0600

    Some changes.

--- src/components/ClosedWeb.jsx
+++ src/components/ClosedWeb.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-function currentDate()
-{
+function currentDate() {
     const month = new Date().getMonth();
     const day = new Date().getDate();
     const year = new Date().getFullYear();
@@ -10,8 +9,7 @@ function currentDate()
 
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    for (var i = 0; i < months.length; i++)
-    {
+    for (var i = 0; i < months.length; i++) {
         if (i === month)
             stringMonth = months[i];
     }
@@ -19,15 +17,13 @@ function currentDate()
     return [stringMonth, " ", day, ", ", year];
 }
 
-function daysLeft()
-{
+function daysLeft() {
     const current_date = new Date().getDate();
     var set_date = 3;
 
     var future_date = Math.abs(current_date - set_date);
 
-    if (future_date === 0)
-    {
+    if (future_date === 0) {
         future_date = "";
     }
 
@@ -44,12 +40,12 @@ const ClosedWeb = () => {
                 <p id="closed-msg">
                     In accordance to the new website operation policy I have implemented in the last month,
                     I decided to close this website on December 19, 2021. I have decided to push the
-                    re-opening date of this website to January 3, 2022 in observance of Christmas and 
+                    re-opening date of this website to January 3, 2022 in observance of Christmas and
                     New Year's.
                 </p>
                 <br />
                 <p id="closed-msg">Have a wonderful holiday and stay safe!</p>
-                <br/>
+                <br />
                 <div id="closed-button">
                     <a href="https://docs.google.com/forms/d/e/1FAIpQLSe1nR8hmOAMRTU3fDZsbLRGkjzKvA9uRLZW_YdvsyBEctqDOw/viewform" rel="noreferrer" target="_blank">Contact Me</a>
                 </div>
