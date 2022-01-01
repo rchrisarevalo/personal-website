86521210a292a63035da8093f7c17d087950c72a
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-01-01 00:13:58 -0600

    Closed route re-established (albeit temporarily).

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -8,7 +8,7 @@ import Spinner from 'react-bootstrap/Spinner';
 const Load = (props) => {
 
     setTimeout(() => {
-        props.history.push('/about')
+        props.history.push('/closed')
     }, 3000)
 
     return (
