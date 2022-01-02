26ffe585df73bc04361daadd12e1eb72a2e0317e
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-01-01 23:55:27 -0600

    React component syntax added.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
 
 // Components that will be used as page routes
 import Load from "./components/Load.jsx";
@@ -17,24 +17,26 @@ import Error404 from "./components/Error404.jsx";
 
 import { Route, Switch } from "react-router-dom";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/" exact component={Load}/>
-        <Route path ="/about" exact component={Intro}/>
-        <Route path="/announcements" exact component={Posts}/>
-        <Route path="/announcements/2021/August" exact component={August2021Posts}/>
-        <Route path="/announcements/2021/September" exact component={September2021Posts}/>
-        <Route path="/announcements/2021/October" exact component={October2021Posts}/>
-        <Route path="/announcements/2021/November" exact component={November2021Posts}/>
-        <Route path="/announcements/2021/December" exact component={December2021Posts}/>
-        <Route path="/archives" exact component={Archives}/>
-        <Route path="/site-operation" exact component={WebsiteOperation}/>
-        <Route exact component={Error404}/>
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Load}/>
+          <Route path ="/about" exact component={Intro}/>
+          <Route path="/announcements" exact component={Posts}/>
+          <Route path="/announcements/2021/August" exact component={August2021Posts}/>
+          <Route path="/announcements/2021/September" exact component={September2021Posts}/>
+          <Route path="/announcements/2021/October" exact component={October2021Posts}/>
+          <Route path="/announcements/2021/November" exact component={November2021Posts}/>
+          <Route path="/announcements/2021/December" exact component={December2021Posts}/>
+          <Route path="/archives" exact component={Archives}/>
+          <Route path="/site-operation" exact component={WebsiteOperation}/>
+          <Route exact component={Error404}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
