1fe3467e7e994535f11901717bb283532c8f7e2a
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-01-03 00:01:57 -0600

    Code bug fixed.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ import Error404 from "./components/Error404.jsx";
 
 import { Route, Switch } from "react-router-dom";
 
-class App extends React.Component {
+class App extends Component {
   render(){
     return (
       <div className="App">
