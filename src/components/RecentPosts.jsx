fa7f07c3a1e45112b0177e414eb87d9846fb3557
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-01-06 01:30:37 -0600

    New recent post added.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,6 +10,18 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-content">
+                    A minor change to the Website Operation policy has been made. There was a typo in the last description
+                    near the footer in the page. That mistake has been corrected.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 01/06/2022 at 1:40 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-content">
                     That's all for now. Have a great rest of the week and please stay safe!
                 </p>
                 <p id="post-content">
@@ -33,28 +45,6 @@ const RecentPosts = () => {
                     as our smart phones, computers, video games, etc. As this website is running on GitHub Pages, I am
                     going to do my best to make sure that this Site does not fall prey to any hacking attempts that might
                     occur in the future, depending on the circumstances surrounding the Log4j vulnerability.
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 01/03/2022 at 12:00 AM.
-                </p>
-            </div>
-            <div className="post" id="post-margin">
-                <p id="post-content">
-                    In addition, I wanted to remind you all that I introduced a new policy that determines what
-                    time this Site can and cannot operate. The new policy was introduced in November 2021, just
-                    before I closed this website for the Thanksgiving holiday. However, I forgot to specifically
-                    outline when and what times the website can operate.
-                </p>
-                <p id="post-content">
-                    Fortunately, I drafted an outline as to how this Site will operate from now on. Keep in mind
-                    that the new policy that is titled "Website Operation Policy" is not a legal document per se,
-                    but rather, to explain what is going to happen to the operations of this Site. The page containing
-                    that policy will explain what circumstances would warrant the closure of this Site, along with
-                    other additional details. For more information, please feel free to click on the "Website Operation"
-                    link on the footer in the "Policies" column.
                 </p>
                 <p id="post-content">
                     -- Ruben Christopher Arevalo
