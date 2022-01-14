1bd709695280531b40d0d934558e5158a3a1b76c
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-01-14 16:09:11 -0600

    Post details added.

--- src/components/2021 Posts Components/September2021Posts.jsx
+++ src/components/2021 Posts Components/September2021Posts.jsx
@@ -32,73 +32,61 @@ const September2021Posts = () => {
                     be deleted or edited unless I decide otherwise.
                 </p>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/30/2021 at 1:06 AM.
+                    </p>
                     <p id="post-content">
                         In addition, this will be the last post for this month. The whole "Announcements"
                         section (with the exception of 'Recent Announcements') will be wiped and replaced
                         with a post (or posts) for the new month (which is October).
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/30/2021 at 1:06 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/30/2021 at 1:06 AM.
+                    </p>
                     <p id="post-content">
                         Hello everyone. I am happy to announce a new UI design for the footer in this page.
                         New information has been included in the copyright notice. This new information
                         should deter any attempts of copyright infringement from people intending to
                         copy this site without permission.
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/30/2021 at 1:06 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/28/2021 at 1:12 AM.
+                    </p>
                     <p id="post-content">
                         Hello everyone. I am leaving this here to let y'all know that I had to remove the description boxes
                         for each of my works in the "About Me" page. I had to do this because I noticed huge styling errors
                         that made it appear worse in other devices. Thankfully, I managed to fix this issue, and even though
                         I am sad to remove the descriptions, it was for the best. I apologize for the inconvenience.
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/28/2021 at 1:12 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/26/2021 at 3:43 PM.
+                    </p>
                     <p id="post-content">
                         Hi everyone! I am leaving this announcement here to let y'all know that this website underwent a UI
                         (user interface) design change once again. I changed the color of the navigation bar, the posts, and
                         redesigned the "Works" section in the "About Me" page. I hope you all enjoy the new design!
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/26/2021 at 3:43 PM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/25/2021 at 11:25 AM.
+                    </p>
                     <p id="post-content">
                         In addition, I reduced the announcement margins for this page only. The ones in the "Recent Announcements"
                         section in the "About Me" page are not affected by this change. Once again, I made this change so that
                         it can be similar to the posts in the "About Me" page.
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/25/2021 at 11:25 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/25/2021 at 11:24 AM.
+                    </p>
                     <p id="post-content">
                         Hi everyone! In case you didn't notice, I renamed the "Contact Information" link to "Contact Info".
                         I shortened it because it was taking up a lot of space on the navigation bar for smaller computers,
@@ -110,41 +98,32 @@ const September2021Posts = () => {
                         the top right (or the message icon on the top right of the mobile version). You can also click on the
                         "Report" button as well. Thank you and have a great day!
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/25/2021 at 11:24 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/21/2021 at 4:09 PM.
+                    </p>
                     <p id="post-content">
                         I am writing this to let y'all know that I expanded the size of the navigation bar for both
                         the web and mobile versions of this site. It's nothing big; just a small change to the
                         UI design of this website.
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/21/2021 at 4:09 PM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/18/2021 at 2:07 AM.
+                    </p>
                     <p id="post-content">
                         Huge announcement! "Posts" has been renamed to "Announcements". The latter seems like a more
                         appropriate title because I only release these posts as official announcements for changes
                         that have occurred or are occurring on this website. I also use it for the holidays
                         whenever I get the chance. I hope you enjoy the new change!
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/18/2021 at 2:07 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/18/2021 at 1:04 AM.
+                    </p>
                     <p id="post-content">
                         Hello everyone! I am here to announce yet another UI change, only this time, the posts appear with a border outline.
                         I added a little bit of touch to the UI of the posts to make it more modern and appealing. There have also been
@@ -160,50 +139,38 @@ const September2021Posts = () => {
                     <p id="post-content">
                         Have a wonderful rest of the day (if you happen to be awake to see this, yes I stay up late most of the time).
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/18/2021 at 1:04 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/15/2021 at 1:12 AM.
+                    </p>
                     <p id="post-content">
                         This will be brief. I updated the website's font to make it more appealing for users visiting
                         this site. I hope you enjoy this update!
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/15/2021 at 1:12 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/14/2021 at 8:39 PM.
+                    </p>
                     <p id="post-content">
                         I am leaving this here to let y'all know that I am doing fine! I am really busy doing
                         college work and I am currently bombarded by a lot of homework. I am trying my best to
                         have some free time between college and maintaining this website.
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/14/2021 at 8:39 PM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/6/2021 at 1:19 AM.
+                    </p>
                     <p id="post-content">
                         Happy Labor Day, everyone! I hope y'all have a wonderful one!
                     </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/6/2021 at 1:19 AM.
-                    </p>
                 </div>
                 <div className="post" id="post-margin">
+                    <p id="post-info">
+                        By: Ruben Christopher Arevalo. Posted on 9/1/2021 at 12:00 AM.
+                    </p>
                     <p id="post-content">
                         I want to make another major announcement regarding my post archival policy. I have decided to
                         archive all posts <b>by the last day of each month</b>. In other words, I will completely wipe off
@@ -215,12 +182,6 @@ const September2021Posts = () => {
                         then the link to view archived posts for this month will be disabled until October. After that deadline
                         has expired, and as I have mentioned before, I will move the posts from that month into their own
                         separate page. Then, the link will be available afterwards.
-                    </p>
-                    <p id="post-content">
-                        -- Ruben Christopher Arevalo
-                    </p>
-                    <p id="post-date">
-                        Posted on 9/1/2021 at 12:00 AM.
                     </p>
                 </div>
             </div>
