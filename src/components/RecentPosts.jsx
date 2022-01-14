c70daa7ad6fe0b2ae475d264aeb968e7587de014
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-01-14 05:21:28 -0600

    New posts added to "Recent Posts" section.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,6 +10,35 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin" data-aos="fade" data-aos-delay="500">
                 <p id="post-content">
+                    Before I forget, I introduced a new policy that can be found in the link
+                    "Archival" in the footer of this Site. Again, it is not a legal document,
+                    but an outline that represents how the archival process of announcements
+                    like these works. If the language in the page happens to confuse you, please
+                    let me know, and I will try my best to make some clarifications to the outline
+                    to make it easier for you and other users to understand.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 01/14/2022 at 5:22 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos="fade" data-aos-delay="500">
+                <p id="post-content">
+                    I made a typo while I made a change to the Website Operation policy. I forgot to update the
+                    date where it was last modified (3 days ago at the time of writing this announcement). It
+                    should be fixed now.
+                </p>
+                <p id="post-content">
+                    -- Ruben Christopher Arevalo
+                </p>
+                <p id="post-date">
+                    Posted on 01/14/2022 at 5:22 AM.
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos="fade" data-aos-delay="500">
+                <p id="post-content">
                     A minor change to the Website Operation policy has been made. There was a typo in the last description
                     near the footer in the page. That mistake has been corrected.
                 </p>
@@ -18,39 +47,6 @@ const RecentPosts = () => {
                 </p>
                 <p id="post-date">
                     Posted on 01/06/2022 at 1:40 AM.
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos="fade" data-aos-delay="500">
-                <p id="post-content">
-                    That's all for now. Have a great rest of the week and please stay safe!
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 01/03/2022 at 12:00 AM.
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos="fade" data-aos-delay="500">
-                <p id="post-content">
-                    Now that I am done covering the new "Website Operation" policy, I also wanted to let y'all know
-                    that I am back and will be maintaining this site as before. However, expect my activity to dwindle
-                    down a little bit on January 18th and afterwards, as the Spring semester of my university starts
-                    that day. Despite that, I will continue to make an effort to maintain this Site and keep it running
-                    without any bugs or any regressions that might be introduced in the future.
-                </p>
-                <p id="post-content">
-                    Since I am speaking about bugs right now, I will also keep an eye out on issues that might be caused
-                    by a vulnerability with Log4j, which is widely used in many pieces of software around the world, such
-                    as our smart phones, computers, video games, etc. As this website is running on GitHub Pages, I am
-                    going to do my best to make sure that this Site does not fall prey to any hacking attempts that might
-                    occur in the future, depending on the circumstances surrounding the Log4j vulnerability.
-                </p>
-                <p id="post-content">
-                    -- Ruben Christopher Arevalo
-                </p>
-                <p id="post-date">
-                    Posted on 01/03/2022 at 12:00 AM.
                 </p>
             </div>
         </div>
