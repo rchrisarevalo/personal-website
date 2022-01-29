b3d4f5d79ef0dc6bc5fa7bb5b53fa75d28236d36
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-01-29 00:33:01 -0600

    Route added for January 2022 archives.

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import September2021Posts from "./components/2021 Posts Components/September2021
 import October2021Posts from "./components/2021 Posts Components/October2021Posts.jsx";
 import November2021Posts from "./components/2021 Posts Components/November2021Posts.jsx";
 import December2021Posts from './components/2021 Posts Components/December2021Posts';
+import January2022Posts from './components/2022 Posts Components/January2022Posts';
 import Archives from "./components/Archives.jsx";
 import WebsiteOperation from "./components/policies/WebsiteOperation.jsx";
 import ArchivePolicy from "./components/policies/ArchivePolicy.jsx";
@@ -40,6 +41,7 @@ const App = () => {
         <Route path="/announcements/2021/October" exact component={October2021Posts} />
         <Route path="/announcements/2021/November" exact component={November2021Posts} />
         <Route path="/announcements/2021/December" exact component={December2021Posts} />
+        <Route path="/announcements/2022/January" exact component={January2022Posts} />
         <Route path="/archives" exact component={Archives} />
         <Route path="/policies/site-operation" exact component={WebsiteOperation} />
         <Route path="/policies/archive" exact component={ArchivePolicy}/>
