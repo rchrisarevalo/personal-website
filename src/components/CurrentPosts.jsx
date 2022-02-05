5eb2f60681d5fd57f49c896f7df952cbba4fa08b
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-02-05 14:56:32 -0600

    New posts added.

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CurrentPosts = () => {
     return (
@@ -11,6 +12,44 @@ const CurrentPosts = () => {
                 last day of each month has come to pass, then this whole page (minus the archive page) will
                 be wiped out completely and be replaced with announcements for the new month.
             </p>
+            <p id="posts-description">
+                <i>
+                    To view past announcements, click on the link that will take you to the <Link to="/archives">Archives</Link> page
+                    (or the link on the navigation bar). The Archives page gets updated every month for the duration of this Site.
+                </i>
+            </p>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 02/05/2022, 3:00 PM.
+                </p>
+                <p id="post-content">
+                    I will continue to make improvements whenever I had time. It seems like I had time today to make these improvements and add a new Settings
+                    page that will allow for the user to decide their experience on this Site. Have a wonderful afternoon, everyone!
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 02/05/2022, 3:00 PM.
+                </p>
+                <p id="post-content">
+                    In other, but unfortunate news, I have sadly had to take down the contact form. From what I have seen, not many people
+                    have been using it since its introduction. Another reason why I took down the link is due to security reasons. People
+                    with malicious intentions might find a loophole with the form and send spam messages that contain links to malicious
+                    sites that I am in no part supporting.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 02/05/2022, 3:00 PM.
+                </p>
+                <p id="post-content">
+                    Good afternoon! I made some significant changes and added a few features (and Settings page) to this Site. Some of these features include
+                    the ability to clear your local storage on this website only, which will save certain features of this site (such as
+                    the one-time message that you get when you first visit this site), a dark/light mode that I have scrapped months earlier
+                    (and now is available after a few improvements), and the ability to save these changes by simply clicking a button that
+                    will allow you to refresh these changes. How cool is that?
+                </p>
+            </div>
             <div className="post" id="post-margin">
                 <p id="post-info">
                     By: Ruben Christopher Arevalo. Posted on 02/04/2022, 7:19 PM.
