75a6e5fd1e3f0aad7142baebcada3282cb617330
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-02-05 14:57:03 -0600

    Spinner color change.

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -14,9 +14,9 @@ const Load = (props) => {
     return (
         <div className="loading-screen">
             <div className="loading-animation">
-                <Spinner animation="grow" variant="white" id="delay-1" />
-                <Spinner animation="grow" variant="white" id="delay-2" />
-                <Spinner animation="grow" variant="white" id="delay-3" />
+                <Spinner animation="grow" variant="secondary" id="delay-1" />
+                <Spinner animation="grow" variant="secondary" id="delay-2" />
+                <Spinner animation="grow" variant="secondary" id="delay-3" />
             </div>
         </div>
     )
