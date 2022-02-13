7334d4c66f2322ac2921bc0d97c1619b0e1465cc
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-02-13 16:10:00 -0600

    Recent Posts section updated.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -9,35 +9,34 @@ const RecentPosts = () => {
                 To view all posts, click on "Announcements" on the navigation bar.
             </p>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 02/05/2022, 3:00 PM.
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 02/13/2022, 4:11 PM.
                 </p>
                 <p id="post-content">
-                    I will continue to make improvements whenever I had time. It seems like I had time today to make these improvements and add a new Settings
-                    page that will allow for the user to decide their experience on this Site. Have a wonderful afternoon, everyone!
+                    I hope you all had a wonderful weekend and have a great Valentine's Day tomorrow!
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 02/05/2022, 3:00 PM.
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 02/13/2022, 4:11 PM.
                 </p>
                 <p id="post-content">
-                    In other, but unfortunate news, I have sadly had to take down the contact form. From what I have seen, not many people
-                    have been using it since its introduction. Another reason why I took down the link is due to security reasons. People
-                    with malicious intentions might find a loophole with the form and send spam messages that contain links to malicious
-                    sites that I am in no part supporting.
+                    In other news, tomorrow is Valentine's Day! I hope you all spend tomorrow with your loved ones in spite of everything
+                    that has been going on (the pandemic, school, work, etc.). If you don't see an announcement from me tomorrow, I am either
+                    busy with college work and/or spending my day with my loved ones. However, while I may not guarantee it, I will post
+                    an announcement here saying "Happy Valentine's Day, everyone!". Again, if this doesn't happen, I will post a similar,
+                    but subsequent announcement post-Valentine's Day.
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 02/05/2022, 3:00 PM.
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 02/13/2022, 4:11 PM.
                 </p>
                 <p id="post-content">
-                    Good afternoon! I made some significant changes and added a few features (and Settings page) to this Site. Some of these features include
-                    the ability to clear your local storage on this website only, which will save certain features of this site (such as
-                    the one-time message that you get when you first visit this site), a dark/light mode that I have scrapped months earlier
-                    (and now is available after a few improvements), and the ability to save these changes by simply clicking a button that
-                    will allow you to refresh these changes. How cool is that?
+                    Good afternoon. I am writing this announcement to let you all know that I will continue to update this Site whenever.
+                    This week has been pretty busy, not going to lie. However, during the weekend, I found some time to update the color
+                    of the scroll bar to make it more visible regardless of the theme color (light or dark theme that was re-introduced
+                    last week and is located in the Settings page).
                 </p>
             </div>
         </div>
