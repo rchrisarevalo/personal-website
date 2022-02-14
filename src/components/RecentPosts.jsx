bcb8ddbe9a89a01dadc913339ff5ef9d067ad435
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-02-14 07:19:29 -0600

    New post.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -5,9 +5,18 @@ const RecentPosts = () => {
         <div className="posts-container-intro">
             <h1 id="posts-title-intro" data-aos="fade-down">Recent Announcements</h1>
             <p id="posts-description-intro" data-aos="fade-right" data-aos-delay="500">
-                The last three recent posts will be posted here. This page will be updated daily.
-                To view all posts, click on "Announcements" on the navigation bar.
+                The last three recent announcements will be posted here. This page will be updated whenever there are new announcements.
+                To view all announcements, click on "Announcements" on the navigation bar.
             </p>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 02/14/2022, 7:22 AM.
+                </p>
+                <p id="post-content">
+                    Happy Valentine's Day, everyone! I hope you all spend time with your loved ones, whether it'd be your family, your friends,
+                    partners, or spouses! Have a wonderful rest of the day!
+                </p>
+            </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info">
                     By: Ruben Christopher Arevalo. Posted on 02/13/2022, 4:11 PM.
@@ -26,17 +35,6 @@ const RecentPosts = () => {
                     busy with college work and/or spending my day with my loved ones. However, while I may not guarantee it, I will post
                     an announcement here saying "Happy Valentine's Day, everyone!". Again, if this doesn't happen, I will post a similar,
                     but subsequent announcement post-Valentine's Day.
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info">
-                    By: Ruben Christopher Arevalo. Posted on 02/13/2022, 4:11 PM.
-                </p>
-                <p id="post-content">
-                    Good afternoon. I am writing this announcement to let you all know that I will continue to update this Site whenever.
-                    This week has been pretty busy, not going to lie. However, during the weekend, I found some time to update the color
-                    of the scroll bar to make it more visible regardless of the theme color (light or dark theme that was re-introduced
-                    last week and is located in the Settings page).
                 </p>
             </div>
         </div>
