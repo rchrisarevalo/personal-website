66183630b57415ade5a15e6c2a57c8d78ddc143f
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-03-05 13:06:18 -0600

    New style changes.

--- src/components/policies/ArchivePolicy.jsx
+++ src/components/policies/ArchivePolicy.jsx
@@ -37,7 +37,7 @@ const ArchivePolicy = () => {
                 <div className="archive-policy-description">
                     <h1>Archival Policy</h1>
                     <br></br>
-                    <h4><i>Last Modified: February 5, 2022</i></h4>
+                    <h4><i>Last modified on: March 5, 2022</i></h4>
                     <br></br>
                     <p>
                         Hello everyone. On September 2021, I started to archive posts from the
@@ -56,7 +56,7 @@ const ArchivePolicy = () => {
                     </p>
                     <p>
                         <i>
-                            <b>This policy will be subject to change at the owner's discretion.</b>
+                            <b>THIS POLICY IS SUBJECT TO CHANGE AT THE OWNER'S DISCRETION.</b>
                         </i>
                     </p>
                     <br></br>
