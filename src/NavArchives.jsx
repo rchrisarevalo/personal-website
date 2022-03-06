618880956b7026ec1e20d06d6372e26641068b98
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-03-06 08:19:00 -0600

    Unselected icons replaced with outlined ones.

--- src/NavArchives.jsx
+++ src/NavArchives.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { IoIosMenu, IoIosInformationCircleOutline } from "react-icons/io";
-import { IoSettingsOutline, IoCloseOutline, IoMegaphoneSharp, IoArchiveSharp } from "react-icons/io5";
+import { IoSettingsOutline, IoCloseOutline, IoMegaphoneOutline, IoArchiveSharp } from "react-icons/io5";
 
 import { Link, HashRouter } from "react-router-dom";
 import ReactDOMServer from 'react-dom/server';
@@ -36,7 +36,7 @@ const NavArchives = () => {
       <ul className="nav-list">
         <HashRouter>
           <li><Link to="/about"><IoIosInformationCircleOutline id="nav-icons-style" size="16px" />About Me</Link></li>
-          <li><Link to="/announcements"><IoMegaphoneSharp id="nav-icons-style" size="16px" />Announcements</Link></li>
+          <li><Link to="/announcements"><IoMegaphoneOutline id="nav-icons-style" size="16px" />Announcements</Link></li>
           <li><Link to="/archives" id="current-link"><IoArchiveSharp id="nav-icons-style" size="16px" /><b>Archives</b></Link></li>
           <li><Link to="/settings"><IoSettingsOutline id="nav-icons-style" size="16px" />Settings</Link></li>
         </HashRouter>
