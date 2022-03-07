e46b314cab8ebfa79db4f766201fc10cb901974e
rchrisarevalo <42070061+soccerguy1020@users.noreply.github.com>
2022-03-07 15:04:49 -0600

    New posts added to 'Recent Announcements' section

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,34 +10,29 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/05/2022, 1:12 PM.
+                    By: Ruben Christopher Arevalo. Posted on 03/07/2022, 3:08 PM.
                 </p>
                 <p id="post-content">
-                    For those of you that haven't noticed, there have been some style changes I made to the UI of this website.
-                    A white border surrounding one of the links is an indicator of the page you are currently in. As usual,
-                    I will continue to maintain this website whenever I have time. Thank you for your patience.
+                    That is all I have for today. I hope you all have a great rest of the week!
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/01/2022, 12:00 AM.
+                    By: Ruben Christopher Arevalo. Posted on 03/07/2022, 3:08 PM.
                 </p>
                 <p id="post-content">
-                    Apart from that, I am nevertheless excited by the idea of creating a blog since it allows me to express
-                    myself a little bit more. I am expected to make a final decision regarding my blog by the end of the month.
-                    That's all the announcements I have for today! Have a great rest of the day!
+                    In other news, spring break is coming up next week, and I will have a little bit more time
+                    to continue to maintain this website provided that I don't have that much homework to begin
+                    with.
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/01/2022, 12:00 AM.
+                    By: Ruben Christopher Arevalo. Posted on 03/07/2022, 3:08 PM.
                 </p>
                 <p id="post-content">
-                    Should I decide to create new blog, it won't probably be ready until October 2022. The reason for
-                    this is because I am going to be a senior in college. When that happens, I will be more busier as
-                    I am going to be building a senior project that I need to do if I want to graduate. Whatever happens,
-                    and if time permits, then I will continue working on the blog. Again, keep in mind that what I mentioned
-                    above will only occur if I decide to create it.
+                    Happy Monday, everyone! I hope you all had a good start to the week so far and will continue to
+                    have a good week!
                 </p>
             </div>
         </div>
