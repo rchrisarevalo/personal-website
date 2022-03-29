60d93ac5f826f65752509f36bcdee801aec74bf6
rchrisarevalo <rubenplayer105@gmail.com>
2022-03-29 08:40:14 -0500

    New post.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,45 +10,32 @@ const RecentPosts = () => {
             </p> 
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/16/2022, 11:54 AM.
+                    By: Ruben Christopher Arevalo. Posted on 03/29/2022, 8:41 AM.
                 </p>
                 <p id="post-content">
-                    I am leaving this here to let you all know that I have experienced big setbacks during the past few days.
-                    My computer was acting up lately during the last few days, and it was a difficult period as I tried
-                    everything to fix the problem that was affecting it. Unfortunately, I had to reset my computer completely
-                    in a last-ditch attempt to fix everything. The good news is that everything is working well. I managed to get
-                    my work for this site back and I am planning to do the same for my CV website as well.
-                </p>
-                <p id="post-content">
-                    I also want to take this opportunity to announce that I am going to <b>delay the decision for the blog until next month. </b>
-                    College work and other personal circumstances have made it difficult for me to keep updating this website at the moment
-                    which is the reasoning of why I made this decision.
-                </p>
-                <p id="post-content">
-                    That's all I have to say for today. Thank you and have a great day!
+                    I hope I will have enough time to continue to update this website. Anyways, thank you all for your patience. Have a
+                    wonderful morning and please stay safe!
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/11/2022, 7:11 PM.
+                    By: Ruben Christopher Arevalo. Posted on 03/29/2022, 8:41 AM.
                 </p>
                 <p id="post-content">
-                    That is all I have for today. Before I conclude this, I would like to put a reminder that a decision
-                    regarding whether or not I should put a blog on my website or create a separate site will be made in
-                    almost 3 weeks from now. However, it could be later than that as I will be more busy than before once
-                    I return to school from spring break. Have a great weekend and spring break everyone. Please stay safe!
+                    In other news, I will announce my decision as to whether or not to create a blog next month. Originally, I was going to
+                    announce by the end of this month. However, as I stated before, I would not able to do so right now because I have to deal
+                    with the last few weeks of this semester. Even though by the end of April I would be prepping for finals, there won't be
+                    much homework aside from a project that I have to work on before the end of that month.
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/11/2022, 7:11 PM.
+                    By: Ruben Christopher Arevalo. Posted on 03/29/2022, 8:41 AM.
                 </p>
                 <p id="post-content">
-                    In addition, I am not going to be updating the website as much as I do during the course of the
-                    spring break. The last 2-3 weeks have been really stressful, as homeworks, quizzes, and tests have
-                    been piling up at a fast rate during that time. I am glad that it's all over with for now. Except
-                    for having one homework to do during the spring break and catching up on a project, I get to enjoy a
-                    well-deserved break along with other people who may have been in the same predicament as me.
+                    Good morning, everyone! I want to let you all know that I am doing well. Last week has been really busy, as there was
+                    back-to-back homework assignments and a test I had to take. The workload should be a little lighter this week, which
+                    means that I might be able to modify this website and archive this month's announcements before the 31st.
                 </p>
             </div>
         </div>
