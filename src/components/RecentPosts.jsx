09e5266a297b963acf762a57417ea4523faf5319
rchrisarevalo <rubenplayer105@gmail.com>
2022-04-01 00:11:59 -0500

    New posts.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -37,33 +37,6 @@ const RecentPosts = () => {
                     kind of lousy. But hey, I tried my best!
                 </p>
             </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/31/2022, 1:30 PM.
-                </p>
-                <p id="post-content">
-                    This month's announcements will be cleared by the end of today (just before midnight).
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/30/2022, 5:52 PM.
-                </p>
-                <p id="post-content">
-                    I am posting this as a reminder that this month's announcements on this page will be cleared tomorrow
-                    before midnight. New announcements for April will be posted following the Archival process of this month's
-                    announcements. Have a great day!
-                </p>
-            </div> 
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 03/29/2022, 8:41 AM.
-                </p>
-                <p id="post-content">
-                    I hope I will have enough time to continue to update this website. Anyways, thank you all for your patience. Have a
-                    wonderful morning and please stay safe!
-                </p>
-            </div>
         </div>
     );
 }
