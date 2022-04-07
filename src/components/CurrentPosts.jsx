9cdc6403e1c4cd172d8d81ded10445e5b3801155
rchrisarevalo <rubenplayer105@gmail.com>
2022-04-07 18:03:31 -0500

    Time changed.

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -20,6 +20,47 @@ const CurrentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 04/07/2022, 5:15 PM.
+                </p>
+                <p id="post-content">
+                    That is everything I have to say. Thank you for your patience and I hope to re-open this website once the timeframe has
+                    passed. Thank you and have a great day!
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 04/07/2022, 5:15 PM.
+                </p>
+                <p id="post-content">
+                    As this is a matter of education, my education in fact, these circumstances qualify for the "Personal Affairs" section to be
+                    invoked. This is probably the first time I had to use the Site Operation policy in times like these. In contrast to the section
+                    stating that the website won't be necessarily shut down, I am going to have to, at least for the next two to three weeks.
+                    This change will take effect on Saturday, April 9, midnight.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 04/07/2022, 5:15 PM.
+                </p>
+                <p id="post-content">
+                    My circumstances have become a little bit complicated. I hope to come back before the end of the month so I can announce whether or
+                    not I will be creating a blog. If not, then I will come back in the first week of May. The workload should be lighter by then, except
+                    for a poster for a presentation that I have to do.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 04/07/2022, 5:15 PM.
+                </p>
+                <p id="post-content">
+                    Good afternoon. I am announcing that I will be taking a leave of absence for the next 2-3 weeks. The reason for why I am taking
+                    this leave of absence is because due to this month being the last full month of the semester before the end of May (when the
+                    semester ends), I am going to have to stay focused now more than ever. I have a very important assignment due at the end of the
+                    semester, on top of dealing with other homework assignments, quizzes, and exams.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
                     By: Ruben Christopher Arevalo. Posted on 04/01/2022, 12:12 AM.
                 </p>
                 <p id="post-content">
