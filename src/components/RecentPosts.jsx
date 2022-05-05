bd8a7ff841a96e2cf8a3a6da8828905c35358b55
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-05 08:10:57 -0500

    Recent posts added.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,28 +10,32 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/02/2022, 10:25 AM.
+                    By: Ruben Christopher Arevalo. Posted on 05/05/2022, 8:14 AM.
                 </p>
                 <p id="post-content">
-                    Other than that, I should be good. I will see you all later! Have a great rest of the week everyone!
+                    Now that I am done with my announcements for today, I hope you all have a wonderful day! I hope to have a good one
+                    as well!
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/02/2022, 10:25 AM.
+                    By: Ruben Christopher Arevalo. Posted on 05/05/2022, 8:14 AM.
                 </p>
                 <p id="post-content">
-                    I am almost done with the semester. I have a final to study for this week, so I am going to divert all of my attention to that
-                    in addition to other pending assignments that I need to complete.
+                    Creating a website like that requires a lot of work, so I am not sure how long it will exactly take, as I need to prep
+                    for my last year at the university and my senior design project. Nevertheless, I will do my <b>VERY BEST</b> to make sure that it comes out as I intend it to be and how I visualize it.
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/02/2022, 10:25 AM.
+                    By: Ruben Christopher Arevalo. Posted on 05/05/2022, 8:14 AM.
                 </p>
                 <p id="post-content">
-                    Good morning, everyone! I apologize for updating this page late. I was really busy this weekend, and I did not have enough time
-                    to get rid of last month's announcements. Regardless, the archive link should be open for the announcements from April 2022.
+                    By <i>partially</i>, I mean I am not going to construct the blog site fully yet. I need to plan out the type of blog
+                    site it will be. I am still in the planning stages yet, and I have to find out how to set up a comment system, as well
+                    as setting up a reaction system as well, similar to the ones used in today's social media applications. And since this
+                    will be my first time setting one up, I thought it'd be best to learn through experience, which may take a while
+                    depending on other external circumstances I might encounter along the way.
                 </p>
             </div>
         </div>
