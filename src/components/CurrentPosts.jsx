f88b38c4e006f82e63d401e87b5c02e118f2352a
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-11 18:56:23 -0500

    New posts.

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -20,6 +20,33 @@ const CurrentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 05/11/2022, 7:01 PM.
+                </p>
+                <p id="post-content">
+                    Those are all the announcements I have for today. Thank you and have a wonderful evening and great rest of the week!
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 05/11/2022, 7:01 PM.
+                </p>
+                <p id="post-content">
+                    In other news, new changes to the user interface in the "Settings" page and in the footer were made a few days ago and today respectively, especially in the mobile version.
+                    When one hovers over the links in the footer, in the "Site Operation" link, the text tends to get out of its borders, which
+                    makes the Site appear ugly and uncared for. Luckily, those problems have been solved, and the site should appear a little bit normal than from before.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 05/11/2022, 7:01 PM.
+                </p>
+                <p id="post-content">
+                    Good evening, everyone! I have some great news to share! As you can see in the bottom of the "About Me" page, there is a
+                    new contact form up and ready to go! I will most likely move the contact form to a new page at a future date.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
                     By: Ruben Christopher Arevalo. Posted on 05/05/2022, 8:14 AM.
                 </p>
                 <p id="post-content">
