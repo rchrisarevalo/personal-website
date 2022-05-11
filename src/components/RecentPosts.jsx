e4ff0b8d00a067f5353f00dda8d090cf6067d145
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-11 18:56:34 -0500

    New posts added to "Recent Announcements".

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,32 +10,29 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/05/2022, 8:14 AM.
+                    By: Ruben Christopher Arevalo. Posted on 05/11/2022, 7:01 PM.
                 </p>
                 <p id="post-content">
-                    Now that I am done with my announcements for today, I hope you all have a wonderful day! I hope to have a good one
-                    as well!
+                    Those are all the announcements I have for today. Thank you and have a wonderful evening and great rest of the week!
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/05/2022, 8:14 AM.
+                    By: Ruben Christopher Arevalo. Posted on 05/11/2022, 7:01 PM.
                 </p>
                 <p id="post-content">
-                    Creating a website like that requires a lot of work, so I am not sure how long it will exactly take, as I need to prep
-                    for my last year at the university and my senior design project. Nevertheless, I will do my <b>VERY BEST</b> to make sure that it comes out as I intend it to be and how I visualize it.
+                    In other news, new changes to the user interface in the "Settings" page and in the footer were made a few days ago and today respectively, especially in the mobile version.
+                    When one hovers over the links in the footer, in the "Site Operation" link, the text tends to get out of its borders, which
+                    makes the Site appear ugly and uncared for. Luckily, those problems have been solved, and the site should appear a little bit normal than from before.
                 </p>
             </div>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/05/2022, 8:14 AM.
+                    By: Ruben Christopher Arevalo. Posted on 05/11/2022, 7:01 PM.
                 </p>
                 <p id="post-content">
-                    By <i>partially</i>, I mean I am not going to construct the blog site fully yet. I need to plan out the type of blog
-                    site it will be. I am still in the planning stages, and I have to find out how to set up a comment system, as well
-                    as setting up a reaction system as well, similar to the ones used in today's social media applications. And since this
-                    will be my first time setting one up, I thought it'd be best to learn through experience, which may take a while
-                    depending on other external circumstances I might encounter along the way.
+                    Good evening, everyone! I have some great news to share! As you can see in the bottom of the "About Me" page, there is a
+                    new contact form up and ready to go! I will most likely move the contact form to a new page at a future date.
                 </p>
             </div>
         </div>
