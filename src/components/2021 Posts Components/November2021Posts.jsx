3a7371e0e56a1397c7adaff9ae6dad2ce626528a
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-11 16:58:49 -0500

    Code refactoring.

--- src/components/2021 Posts Components/November2021Posts.jsx
+++ src/components/2021 Posts Components/November2021Posts.jsx
@@ -19,11 +19,6 @@ const November2021Posts = () => {
                 <title>Ruben C. Arevalo - Personal Website - Announcements (November 2021)</title>
             </Helmet>
             <NavArchives />
-            <div className="notification" id="warning-notif-2">
-                <div className="notification-content" id="notif-2">
-                    <p>Redirecting to contact form page...</p>
-                </div>
-            </div>
             <div class="posts-container">
                 <HashRouter>
                     <Link to="/announcements/2021/October"><IoArrowBackCircleOutline id="posts-arrow-left" title="October 2021 Announcements" size="40px"/></Link>
