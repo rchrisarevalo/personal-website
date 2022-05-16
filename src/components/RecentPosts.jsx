985e05295d5811fe0b0f13cbcfa9c169ae9837cd
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-15 23:03:00 -0500

    New announcements.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,6 +10,15 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
+                    By: Ruben Christopher Arevalo. Posted on 05/15/2022, 11:06 PM.
+                </p>
+                <p id="post-content">
+                    Good evening. I am writing this announcement to share that I have included the link to my CV website. The link can be
+                    located in the front page (the "About Me" page) of the site. As always, have a wonderful rest of the upcoming week!
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info-recent">
                     By: Ruben Christopher Arevalo. Posted on 05/11/2022, 7:01 PM.
                 </p>
                 <p id="post-content">
@@ -24,15 +33,6 @@ const RecentPosts = () => {
                     In other news, new changes to the user interface in the "Settings" page and in the footer were made a few days ago and today respectively, especially in the mobile version.
                     When one hovers over the links in the footer, in the "Site Operation" link, the text tends to get out of its borders, which
                     makes the Site appear ugly and uncared for. Luckily, those problems have been solved, and the site should appear a little bit normal than from before.
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/11/2022, 7:01 PM.
-                </p>
-                <p id="post-content">
-                    Good evening, everyone! I have some great news to share! As you can see in the bottom of the "About Me" page, there is a
-                    new contact form up and ready to go! I will most likely move the contact form to a new page at a future date.
                 </p>
             </div>
         </div>
