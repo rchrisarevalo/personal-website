97cdb91733936cacb63851cafa7d53807c1cd655
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-20 21:04:26 -0500

    Recent posts removed.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -36,37 +36,6 @@ const RecentPosts = () => {
                     "About Me" page.
                 </p>
             </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/20/2022, 6:42 PM.
-                </p>
-                <p id="post-content">
-                    I am going to take a small break for the next 2 weeks as I want to get rest. However, this does not mean that I will shut down
-                    this website completely. I will <i>occasionally</i> update this site whenever I feel like doing so. I should be back in full
-                    energy after the aforementioned time period. Anyways, and again, have a wonderful summer, and I hope to reach out again!
-                </p>
-            </div>
-            <div className="post" id="post-margin">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/20/2022, 6:42 PM.
-                </p>
-                <p id="post-content">
-                    Hi everyone! As of right now, I am currently doing nothing in terms of resting and regaining my energy after a difficult
-                    semester of full of hard work and stress. Therefore, I might not be as active as I was before, as I want to get my mind ready to
-                    do various things, such as introducing new features to this site, partially set up my personal blog site, etc.
-                    For those of you who are in college, I hope you all have a wonderful summer!
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 05/17/2022, 1:41 PM.
-                </p>
-                <p id="post-content">
-                    Good afternoon. I am writing this announcement to let y'all know that I changed the icon of the website. The icon
-                    of this page resembles a palm tree from one of the pictures that I took and displayed in the "About Me" page. I
-                    hope you all enjoy it!
-                </p>
-            </div>
         </div>
     );
 }
