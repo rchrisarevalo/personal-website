f5e4a3183aec58478ad28fc2aa499cddb07d53fd
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-20 20:59:45 -0500

    New announcements.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,6 +10,34 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
+                    By: Ruben Christopher Arevalo. Posted on 05/20/2022, 9:04 PM.
+                </p>
+                <p id="post-content">
+                    Aside from a few style changes made to this site, this is everything I have to share for the day. Have a great rest of the weekend and please
+                    stay safe!
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info-recent">
+                    By: Ruben Christopher Arevalo. Posted on 05/20/2022, 9:04 PM.
+                </p>
+                <p id="post-content">
+                    Furthermore, I added a graduation date countdown. The reason for this is because I am graduating next year, and I thought it'd be best
+                    to let others know when I am going to graduate, which is May of next year (2023).
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info-recent">
+                    By: Ruben Christopher Arevalo. Posted on 05/20/2022, 9:04 PM.
+                </p>
+                <p id="post-content">
+                    Good evening, everyone! I know I have said that I was going to take a small break, but today, I felt like adding new stuff
+                    to my site and making some changes, such as adding a new, separate page for the contact form that was originally in the
+                    "About Me" page.
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info-recent">
                     By: Ruben Christopher Arevalo. Posted on 05/20/2022, 6:42 PM.
                 </p>
                 <p id="post-content">
