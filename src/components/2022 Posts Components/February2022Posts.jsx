ee9ac362d10d6c0dd7efd14e61d3b1cb1c3c1827
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-24 18:19:28 -0500

    Code refactoring

--- src/components/2022 Posts Components/February2022Posts.jsx
+++ src/components/2022 Posts Components/February2022Posts.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Helmet } from "react-helmet";
-import { Link, HashRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { IoArrowBackCircleOutline, IoArrowForwardCircleOutline } from "react-icons/io5";
 
@@ -20,10 +20,8 @@ const February2022Posts = () => {
             </Helmet>
             <NavArchives />
             <div class="posts-container">
-                <HashRouter>
-                    <Link to="/announcements/2022/January"><IoArrowBackCircleOutline id="posts-arrow-left" title="January 2022 Announcements" size="40px" /></Link>
-                    <Link to="/announcements/2022/March"><IoArrowForwardCircleOutline id="posts-arrow-right" title="March 2022 Announcements" size="40px" /></Link>
-                </HashRouter>
+                <Link to="/announcements/2022/January"><IoArrowBackCircleOutline id="posts-arrow-left" title="January 2022 Announcements" size="40px" /></Link>
+                <Link to="/announcements/2022/March"><IoArrowForwardCircleOutline id="posts-arrow-right" title="March 2022 Announcements" size="40px" /></Link>
                 <h1 id="posts-title">Announcements (February 2022)</h1>
                 <br></br>
                 <p id="posts-description">
