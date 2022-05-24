402f5a4ea000f33a01a5cb24f9cdf365dbb985b4
rchrisarevalo <rubenplayer105@gmail.com>
2022-05-24 18:14:26 -0500

    Different loading variant colors are now available

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 
 import Spinner from 'react-bootstrap/Spinner';
 
-const Load = (props) => {
+const theme = localStorage.getItem("d_l_mode");
 
-    setTimeout(() => {
-        props.history.push('/about')
-    }, 3000)
+const Load = () => {
+
+    const nav = useNavigate();
+
+    useEffect(() => {
+        setTimeout(() => {
+            nav("/about")
+        }, 3000)
+    });
 
     return (
         <div className="loading-screen">
             <div className="loading-animation">
-                <Spinner animation="grow" variant="secondary" id="delay-1" />
-                <Spinner animation="grow" variant="secondary" id="delay-2" />
-                <Spinner animation="grow" variant="secondary" id="delay-3" />
+                { (theme === "normal") && 
+                    <div>
+                        <Spinner animation="grow" variant="dark" id="delay-1" />
+                        <Spinner animation="grow" variant="dark" id="delay-2" />
+                        <Spinner animation="grow" variant="dark" id="delay-3" />
+                    </div>
+                }
+                { (theme === "changed") && 
+                    <div>
+                        <Spinner animation="grow" variant="light" id="delay-1" />
+                        <Spinner animation="grow" variant="light" id="delay-2" />
+                        <Spinner animation="grow" variant="light" id="delay-3" />
+                    </div>
+                }
             </div>
         </div>
     )
