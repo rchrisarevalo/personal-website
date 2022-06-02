cf2aa94d5e508e8919e2bb4930e4c35bccb354ce
rchrisarevalo <rubenplayer105@gmail.com>
2022-06-02 05:59:34 -0500

    new announcement

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -20,6 +20,29 @@ const CurrentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 06/02/2022, 6:01 AM.
+                </p>
+                <p id="post-content">
+                    Fortunately, that bug managed to be fixed, and it is safe to say that the progress countdown
+                    feature should be working as intended. If there are any issues, feel free to contact me
+                    by clicking the "Contact Me" link in the navigation bar! Have a wonderful rest of the day!
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 06/02/2022, 6:01 AM.
+                </p>
+                <p id="post-content">
+                    Good morning, everyone! I know it's a little bit early for me to be posting announcements
+                    at this time. I am writing this announcement to let y'all know that I made some fixes to
+                    the graduation countdown feature in the "About Me" page. Apparently, while I was testing
+                    the countdown feature, the progress percentage that is shown below the progress bar
+                    gave me unusual numbers, such as over 1000% whenever I set today's date a day before
+                    the graduation date.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
                     By: Ruben Christopher Arevalo. Posted on 06/01/2022, 12:00 AM.
                 </p>
                 <p id="post-content">
