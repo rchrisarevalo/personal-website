e575bcf9289597d70a69252f3609896634fae51f
rchrisarevalo <rubenplayer105@gmail.com>
2022-06-03 17:51:11 -0500

    Added rendering if statements for availability.

--- src/App.js
+++ src/App.js
@@ -43,20 +43,23 @@ function App() {
         <Route index path="/" element={<Load />} />
         <Route path="/about" element={<Intro />} />
         <Route path="/announcements" element={<Posts />} />
-        <Route path="/announcements/2021/August" element={<August2021Posts />} />
-        <Route path="/announcements/2021/September" element={<September2021Posts />} />
-        <Route path="/announcements/2021/October" element={<October2021Posts />} />
-        <Route path="/announcements/2021/November" element={<November2021Posts />} />
-        <Route path="/announcements/2021/December" element={<December2021Posts />} />
-        <Route path="/announcements/2022/January" element={<January2022Posts />} />
-        <Route path="/announcements/2022/February" element={<February2022Posts />} />
-        <Route path="/announcements/2022/March" element={<March2022Posts />} />
-        <Route path="/announcements/2022/April" element={<April2022Posts />} />
-        { (Date.now() >= new Date(2022, 5, 1, 0)) &&
-          <Route path="/announcements/2022/May" element={<May2022Posts />} />
-        }
         <Route path="/contact" element={<Contact />} />
         <Route path="/archives" element={<Archives />} />
+
+        {/* 2021 archive routes (expire on December 31, 2024) */}
+        {(Date.now() >= new Date(2021, 0, 1) && Date.now() <= new Date(2024, 11, 31)) && <Route path="/announcements/2021/August" element={<August2021Posts />} />}
+        {(Date.now() >= new Date(2021, 0, 1) && Date.now() <= new Date(2024, 11, 31)) && <Route path="/announcements/2021/September" element={<September2021Posts />} />}
+        {(Date.now() >= new Date(2021, 0, 1) && Date.now() <= new Date(2024, 11, 31)) && <Route path="/announcements/2021/October" element={<October2021Posts />} />}
+        {(Date.now() >= new Date(2021, 0, 1) && Date.now() <= new Date(2024, 11, 31)) && <Route path="/announcements/2021/November" element={<November2021Posts />} />}
+        {(Date.now() >= new Date(2021, 0, 1) && Date.now() <= new Date(2024, 11, 31)) && <Route path="/announcements/2021/December" element={<December2021Posts />} />}
+
+        {/* 2022 archive routes (expire on December 31, 2025) */}
+        {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/January" element={<January2022Posts />} />}
+        {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/February" element={<February2022Posts />} />}
+        {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/March" element={<March2022Posts />} />}
+        {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/April" element={<April2022Posts />} />}
+        {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/May" element={<May2022Posts />} />}
+
         <Route path="/settings" element={<Settings />} />
         <Route path="/policies/site-operation" element={<WebsiteOperation />} />
         <Route path="/policies/archive" element={<ArchivePolicy />} />
