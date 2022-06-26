e6d796b3798b14477c6f25a542d0b56b37f8e3a1
rchrisarevalo <rubenplayer105@gmail.com>
2022-06-26 00:38:40 -0500

    New announcement

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,29 +10,29 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
+                    By: Ruben Christopher Arevalo. Posted on 06/26/2022, 12:40 AM.
+                </p>
+                <p id="post-content">
+                    That is everything I have to share for today. Have a wonderful rest of the weekend
+                    and see you all later!
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info-recent">
+                    By: Ruben Christopher Arevalo. Posted on 06/26/2022, 12:40 AM.
+                </p>
+                <p id="post-content">
+                    Good morning, everyone! I am writing this to let y'all know that on the contact form,
+                    anyone can resize the text box to any size they desire. I decided to include it back
+                    again as some users might have difficulty reading the contents of their message.
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info-recent">
                     By: Ruben Christopher Arevalo. Posted on 06/23/2022, 11:41 PM.
                 </p>
                 <p id="post-content">
                     If there are any problems, please let me know by contacting me in the "Contact Me" page. Have a wonderful evening!
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 06/23/2022, 11:41 PM.
-                </p>
-                <p id="post-content">
-                    Furthermore, the automatic resizing of the window in the mobile version of the website has been corrected.
-                    The font size has been adjusted to limit the resizing bug.
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 06/23/2022, 11:41 PM.
-                </p>
-                <p id="post-content">
-                    I have made some changes to the user interface of the page. The navigation bar has been changed, 
-                    as well as the color of the buttons in the popup messages that appear whenever one wants to
-                    change the settings for the website.
                 </p>
             </div>
         </div>
