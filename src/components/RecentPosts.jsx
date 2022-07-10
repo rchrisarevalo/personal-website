b0d479ac5c8d5ee825bd7f5b1080a67a1c2dce5a
rchrisarevalo <rubenplayer105@gmail.com>
2022-07-10 01:21:59 -0500

    New announcement.

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -10,6 +10,29 @@ const RecentPosts = () => {
             </p>
             <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
                 <p id="post-info-recent">
+                    By: Ruben Christopher Arevalo. Posted on 07/10/2022, 1:23 AM.
+                </p>
+                <p id="post-content">
+                    I am still working on making sure that some aspects of this website, as I have stated
+                    in previous announcements, are automatically updated (for instance, the progress
+                    countdown feature). That will be all for this evening. Thank you everyone, and have a
+                    wonderful rest of the weekend!
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info-recent">
+                    By: Ruben Christopher Arevalo. Posted on 07/10/2022, 1:23 AM.
+                </p>
+                <p id="post-content">
+                    Good evening, everyone! For the past week, I have been making bug fixes on the
+                    progress countdown feature in the "About Me" page (the first page the user is
+                    directed to when entering the website the first time). Everything should be fixed,
+                    but I cannot make any promises when I say that as other bugs can arise in the
+                    future.
+                </p>
+            </div>
+            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
+                <p id="post-info-recent">
                     By: Ruben Christopher Arevalo. Posted on 07/03/2022, 8:34 PM.
                 </p>
                 <p id="post-content">
@@ -17,27 +40,6 @@ const RecentPosts = () => {
                     feel free to fill the contact form by clicking the "Contact Me"
                     link in the navigation bar on the top, or by clicking the three
                     dashed lines in the mobile version of this site.
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 07/03/2022, 8:34 PM.
-                </p>
-                <p id="post-content">
-                    Furthermore, I will be adding some new features sometime soon in a few months
-                    or so. That will be all the announcements for this evening.
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 07/03/2022, 8:34 PM.
-                </p>
-                <p id="post-content">
-                    Good evening. This will be the first announcement for this month. As I have
-                    stated in the previous three announcements, I will be updating this website
-                    so that certain components will be updated automatically instead of being
-                    manually configured (i.e progress bar, number of days left until graduation,
-                    etc.).
                 </p>
             </div>
         </div>
