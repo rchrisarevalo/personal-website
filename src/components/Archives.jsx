a207bba1666f50cc787b2a50b249419477bf27de
rchrisarevalo <rubenplayer105@gmail.com>
2022-07-31 00:33:07 -0500

    Link added to July 2022 archive

--- src/components/Archives.jsx
+++ src/components/Archives.jsx
@@ -66,7 +66,8 @@ const Archives = () => {
                         <li><Link to="/announcements/2022/March">March</Link></li>
                         <li><Link to="/announcements/2022/April">April</Link></li>
                         <li><Link to="/announcements/2022/May">May</Link></li>
-                        {(Date.now() >= new Date(2022, 6, 1, 0) && Date.now() <= new Date(2025, 11, 31)) && <li><Link to="/announcements/2022/June">June</Link></li>}
+                        <li><Link to="/announcements/2022/June">June</Link></li>
+                        {(Date.now() >= new Date(2022, 7, 1, 0) && Date.now() <= new Date(2025, 11, 31)) && <li><Link to="/announcements/2022/July">July</Link></li>}
                     </div>
                     <br></br>
                     <br></br>
