42de44c98e6f3c9611df62fccbe594ba71121f50
rchrisarevalo <rubenplayer105@gmail.com>
2022-07-31 00:33:41 -0500

    Forward arrow added to new July 2022 archive

--- src/components/2022 Posts Components/June2022Posts.jsx
+++ src/components/2022 Posts Components/June2022Posts.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 
-import { IoArrowBackCircleOutline } from "react-icons/io5";
+import { IoArrowBackCircleOutline, IoArrowForwardCircleOutline } from "react-icons/io5";
 
 import NavArchives from "../../NavArchives.jsx";
 import NewFooter from "../../NewFooter.jsx";
@@ -25,6 +25,13 @@ const June2022Posts = () => {
                     <IoArrowBackCircleOutline
                         id="posts-arrow-left"
                         title="May 2022 Announcements"
+                        size="40px"
+                    />
+                </Link>
+                <Link to="/announcements/2022/July">
+                    <IoArrowForwardCircleOutline
+                        id="posts-arrow-right"
+                        title="July 2022 Announcements"
                         size="40px"
                     />
                 </Link>
