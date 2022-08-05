da397d881617a8cf11b0353f0ca2c4e057e7a373
rchrisarevalo <rubenplayer105@gmail.com>
2022-08-05 14:33:33 -0500

    New announcements

--- src/components/CurrentPosts.jsx
+++ src/components/CurrentPosts.jsx
@@ -20,6 +20,35 @@ const CurrentPosts = () => {
             </p>
             <div className="post" id="post-margin">
                 <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 08/05/2022, 2:34 PM.
+                </p>
+                <p id="post-content">
+                    That's all I wanted to announce for today, as it is only a change to the way the Save button
+                    works when, again, making changes to the settings in the 'Settings' page. Thank you and have
+                    a wonderful weekend, y'all!
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 08/05/2022, 2:34 PM.
+                </p>
+                <p id="post-content">
+                    In addition, when making the change and selecting the original setting that was either pre-configured by
+                    default or by yourself, the Save button will be disabled until you select a different one.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
+                    By: Ruben Christopher Arevalo. Posted on 08/05/2022, 2:34 PM.
+                </p>
+                <p id="post-content">
+                    Good afternoon, everyone. I am writing this to announce that I made a few upgrades when saving changes
+                    in the 'Settings' page. When clicking the 'Save' button without actually making any changes to it will
+                    return an error message saying that you have to make them before clicking on the Save button once more.
+                </p>
+            </div>
+            <div className="post" id="post-margin">
+                <p id="post-info">
                     By: Ruben Christopher Arevalo. Posted on 08/01/2022, 12:00 AM.
                 </p>
                 <p id="post-content">
