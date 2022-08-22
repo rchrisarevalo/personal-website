6231c3681fcc65c08dada5ce5e7572ccdeea68f6
rchrisarevalo <rubenplayer105@gmail.com>
2022-08-22 04:30:00 -0500

    Current three posts retrieved by API

--- src/components/RecentPosts.jsx
+++ src/components/RecentPosts.jsx
@@ -1,6 +1,115 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import axios from "axios";
+import db from "./database/posts.json"
 
 const RecentPosts = () => {
+
+    const [currentPostNum, setCurrentPostNum] = useState(0)
+    const [prevPostNum, setPrevPostNum] = useState(currentPostNum)
+
+    useEffect(() => {
+        // Display the last three recent posts
+        axios.get("https://rchrisarevalo.github.io/posts.json", db.post).then((res) => {
+            setPrevPostNum(db.post.length)
+            setCurrentPostNum(db.post.length)
+            console.log("Previous post number: ", prevPostNum)
+            console.log("Current post number: ", currentPostNum)
+            if (currentPostNum === prevPostNum) {
+                for (var i = db.post.length - 1; i >= db.post.length - 3; i--) {
+                    var grandparentDiv = document.getElementById("post-catalogue")
+                    var parentDiv;
+                    var childDiv;
+                    var childDiv2Text;
+                    parentDiv = document.createElement('div')
+                    childDiv = document.createElement('p')
+
+                    childDiv2Text = document.createElement('p')
+                    childDiv2Text.textContent = db.post[i].title
+                    childDiv2Text.style.fontSize = '14px'
+                    childDiv2Text.style.fontStyle = 'italic'
+                    childDiv2Text.style.textAlign = 'left'
+                    childDiv2Text.margin = '.7em'
+
+                    childDiv.textContent = db.post[i].postContent
+                    childDiv.style.fontSize = '16.5px'
+                    childDiv.style.margin = '2em'
+                    childDiv.style.textAlign = 'left'
+
+                    childDiv2Text.setAttribute('id', 'post-info-recent')
+                    childDiv.setAttribute('id', 'post-content')
+
+                    parentDiv.style.background = '#3a3a3a'
+                    parentDiv.style.boxShadow = '0 0 4px black'
+                    parentDiv.style.fontWeight = '200'
+                    parentDiv.style.color = 'white'
+                    parentDiv.style.borderRadius = '5px'
+                    parentDiv.style.margin = '2em'
+                    parentDiv.style.textAlign = 'left'
+                    parentDiv.style.marginLeft = '10%'
+                    parentDiv.style.marginRight = '10%'
+                    parentDiv.style.paddingTop = '10px'
+                    parentDiv.style.paddingBottom = '12px'
+                    parentDiv.style.fontSize = '16.5px'
+
+                    parentDiv.style.marginBottom = '50px'
+                    parentDiv.style.marginTop = '50px'
+
+                    parentDiv.appendChild(childDiv2Text)
+                    parentDiv.appendChild(childDiv)
+                    parentDiv.setAttribute('id', `post${i}`)
+                    parentDiv.setAttribute('class', 'post')
+                    grandparentDiv.appendChild(parentDiv)
+                    setPrevPostNum(db.post.length)
+                    setCurrentPostNum(db.post.length + 1)
+                }
+            }
+            else if (currentPostNum > prevPostNum) {
+                parentDiv = document.createElement('div')
+                childDiv = document.createElement('p')
+
+                console.log("This works!")
+
+                childDiv2Text = document.createElement('p')
+                childDiv2Text.textContent = db.post[currentPostNum - 1].title
+                childDiv2Text.style.fontSize = '14px'
+                childDiv2Text.style.fontStyle = 'italic'
+                childDiv2Text.style.textAlign = 'left'
+                childDiv2Text.margin = '.7em'
+
+                childDiv.textContent = db.post[currentPostNum - 1].postContent
+                childDiv.style.fontSize = '16.5px'
+                childDiv.style.margin = '2em'
+                childDiv.style.textAlign = 'left'
+
+                childDiv2Text.setAttribute('id', 'post-info-recent')
+                childDiv.setAttribute('id', 'post-content')
+
+                parentDiv.style.background = '#3a3a3a'
+                parentDiv.style.boxShadow = '0 0 4px black'
+                parentDiv.style.fontWeight = '200'
+                parentDiv.style.color = 'white'
+                parentDiv.style.borderRadius = '5px'
+                parentDiv.style.margin = '2em'
+                parentDiv.style.textAlign = 'left'
+                parentDiv.style.marginLeft = '10%'
+                parentDiv.style.marginRight = '10%'
+                parentDiv.style.paddingTop = '10px'
+                parentDiv.style.paddingBottom = '12px'
+                parentDiv.style.fontSize = '16.5px'
+                parentDiv.style.marginBottom = '50px'
+                parentDiv.style.marginTop = '50px'
+
+                parentDiv.appendChild(childDiv2Text)
+                parentDiv.appendChild(childDiv)
+                parentDiv.setAttribute('id', `post${currentPostNum - 1}`)
+                document.getElementById("post-catalogue").insertBefore(parentDiv, document.getElementById(`post${currentPostNum - 2}`))
+                setPrevPostNum(parseInt(prevPostNum) + parseInt(1))
+            }
+        })
+    }, [])
+
     return (
         <div className="posts-container-intro">
             <h1 id="posts-title-intro" data-aos="fade-down">Recent Announcements</h1>
@@ -8,35 +117,7 @@ const RecentPosts = () => {
                 The last three recent announcements will be posted here. This page will be updated whenever there are new announcements.
                 To view all announcements, click on "Announcements" on the navigation bar.
             </p>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 08/14/2022, 4:11 PM.
-                </p>
-                <p id="post-content">
-                    I'll let y'all know when it is ready so that way, everything comes out the way it is supposed to.
-                    There will still be bugs, but at a reduced level. That is everything for today. Thank y'all for your
-                    patience and have a wonderful Sunday!
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 08/14/2022, 4:11 PM.
-                </p>
-                <p id="post-content">
-                    More fixes will be included in the next week or the following week, with more focus being put in
-                    how long has it been since the previous update, as the section does not take minutes into consideration.
-                </p>
-            </div>
-            <div className="post" id="post-margin" data-aos-delay="500" data-aos="fade">
-                <p id="post-info-recent">
-                    By: Ruben Christopher Arevalo. Posted on 08/14/2022, 4:11 PM.
-                </p>
-                <p id="post-content">
-                    Good afternoon. I am writing this to announce that I have made a few changes to the progress countdown
-                    section in the "About Me" page. Some of these changes include fixing when the counter should update, as well
-                    as upgrading the design of the progress countdown feature itself.
-                </p>
-            </div>
+            <div id="post-catalogue" data-aos="fade-up" data-aos-delay="500"></div>
         </div>
     );
 }
