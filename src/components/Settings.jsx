77f3e6147f55bdde6041f6d8837258b94ca500ea
rchrisarevalo <rubenplayer105@gmail.com>
2022-12-23 12:18:45 -0600

    Christmas theme added.

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -17,6 +17,11 @@ var one_time_message = localStorage.getItem("one-time")
 var saved_status = false;
 var theme_choice;
 
+var christmasThemeList = ['#ffffff', '#e20c0c', '#0ee812', '#ffffff', '#e20c0c', '#0ee812', '#ffffff', '#e20c0c', '#0ee812', '#ffffff', '#e20c0c', '#0ee812']
+var christmasColorList = ['#e20c0c', '#ffffff', '#e20c0c', '#e20c0c', '#ffffff', '#e20c0c', '#e20c0c', '#ffffff', '#e20c0c', '#e20c0c', '#ffffff', '#e20c0c']
+
+var randomIndex = Math.floor(Math.random() * 11) + 0
+
 function clearLocalStorage() {
     var clearNotice = document.getElementById("clear-notice");
     var changesSaved = document.getElementById("clear-storage-msg");
@@ -44,6 +49,8 @@ function light_dark_mode() {
     var save_button_status = document.getElementById("save-btn")
     var prevState;
 
+    randomIndex = Math.floor(Math.random() * 2) + 0
+
     if (theme_choice === "dark" && prevState !== d) {
         prevState = d
         document.body.style.cssText = 'background: #242525; color: white; transition: .5s;'
@@ -51,6 +58,7 @@ function light_dark_mode() {
         localStorage.setItem("dark_media_theme", "false")
         localStorage.setItem("light_media_theme", "false")
         localStorage.setItem("save_status", "saved")
+
     } else if (theme_choice === "light" && prevState !== d) {
         prevState = d
         document.body.style.cssText = 'background: #FFFFFF; color: black; transition: .5s;'
@@ -58,13 +66,15 @@ function light_dark_mode() {
         localStorage.setItem("dark_media_theme", "false")
         localStorage.setItem("light_media_theme", "false")
         localStorage.setItem("save_status", "saved")
-    } else if (theme_choice === "thanksgiving" && prevState !== d) {
+
+    } else if (theme_choice === "christmas" && prevState !== d) {
         prevState = d
-        document.body.style.cssText = 'background: #BE5634; color: black; transition: .5s;'
+        document.body.style.cssText = `background: ${christmasThemeList[randomIndex]}; color: ${christmasColorList[randomIndex]}; transition: .5s;`
         localStorage.setItem("d_l_mode", theme_choice)
         localStorage.setItem("dark_media_theme", "false")
         localStorage.setItem("light_media_theme", "false")
         localStorage.setItem("save_item", "saved")
+
     } else if (theme_choice === "default" && prevState !== d) {
         if (window.matchMedia("(prefers-color-scheme: light)").matches) {
             prevState = d
@@ -74,6 +84,7 @@ function light_dark_mode() {
             localStorage.setItem("light_media_theme", "true")
             localStorage.setItem("dark_media_theme", "false")
             localStorage.setItem("save_status", "saved")
+
         } else {
             prevState = d
             document.body.style.cssText = 'background: #242525; color: white; transition: .5s;'
@@ -101,6 +112,7 @@ function light_dark_mode() {
 
 if (d === "dark" || d === "default_dark") {
     document.body.style.cssText = 'background: #242525; color: white; transition: .5s;'
+
 } else if (l_media === "true" || d_media === "true" || (l_media === null && d_media === null)){
     if (window.matchMedia("(prefers-color-scheme: dark)").matches){
         document.body.style.cssText = 'background: #242525; color: white; transition: .5s;'
@@ -112,10 +124,12 @@ if (d === "dark" || d === "default_dark") {
         localStorage.setItem("dark_media_theme", "false")
         localStorage.setItem("light_media_theme", "true")
     }
+
 } else if (d === "light" || d === "default_light") {
     document.body.style.cssText = 'background: #FFFFFF; color: black; transition: .5s;'
-} else if (d === "thanksgiving") {
-    document.body.style.cssText = 'background: #BE5634; color: black; transition: .5s;'
+
+} else if (d === "christmas") {
+    document.body.style.cssText = `background: ${christmasThemeList[randomIndex]}; color: ${christmasColorList[randomIndex]}; transition: .5s;`
 }
 
 // ====================================
@@ -179,12 +193,6 @@ const Settings = () => {
     const handleNoticeClose = () => setNoticeShow(false);
     const handleNoticeShow = () => setNoticeShow(true);
 
-    const [dateState, setDateState] = useState(new Date())
-
-    setTimeout(() => {
-        setDateState(new Date())
-    }, 1000)
-
     return (
         <div className="settings-container">
             <NavSettings />
@@ -220,8 +228,8 @@ const Settings = () => {
                                 <option value="default" selected>Device theme</option>
                                 <option value="light">Light theme</option>
                                 <option value="dark">Dark theme</option>
-                                { (Date.now() >= new Date(2022, 10, 15) && Date.now() <= new Date(2022, 10, 25, 0, 45)) && 
-                                     <option value="thanksgiving">Thanksgiving theme</option>
+                                { (Date.now() >= new Date(2022, 11, 23) && Date.now() <= new Date(2022, 11, 26)) && 
+                                     <option value="christmas">Christmas theme</option>
                                 }
                             </select>
                         }
@@ -230,8 +238,8 @@ const Settings = () => {
                                 <option value="default">Device theme</option>
                                 <option value="light">Light theme</option>
                                 <option value="dark" selected>Dark theme</option>
-                                { (Date.now() >= new Date(2022, 10, 15) && Date.now() <= new Date(2022, 10, 25, 0, 45)) && 
-                                     <option value="thanksgiving">Thanksgiving theme</option>
+                                { (Date.now() >= new Date(2022, 11, 23) && Date.now() <= new Date(2022, 11, 26)) && 
+                                     <option value="christmas">Christmas theme</option>
                                 }
                             </select>
                         }
@@ -240,17 +248,17 @@ const Settings = () => {
                                 <option value="default">Device theme</option>
                                 <option value="light" selected>Light theme</option>
                                 <option value="dark">Dark theme</option>
-                                { (Date.now() >= new Date(2022, 10, 15) && Date.now() <= new Date(2022, 10, 25, 0, 45)) && 
-                                     <option value="thanksgiving">Thanksgiving theme</option>
+                                { (Date.now() >= new Date(2022, 11, 23) && Date.now() <= new Date(2022, 11, 26)) && 
+                                     <option value="christmas">Christmas theme</option>
                                 }
                             </select>
                         }
-                        {d === "thanksgiving" && (Date.now() >= new Date(2022, 10, 15) && Date.now() <= new Date(2022, 10, 25, 0, 45)) &&
+                        {d === "christmas" && (Date.now() >= new Date(2022, 11, 23) && Date.now() <= new Date(2022, 11, 26)) &&
                             <select id="value_check" onChange={light_dark_mode}>
                                 <option value="default">Device theme</option>
                                 <option value="light">Light theme</option>
                                 <option value="dark">Dark theme</option>
-                                <option value="thanksgiving" selected>Thanksgiving theme</option>
+                                <option value="christmas" selected>Christmas theme</option>
                             </select>
                         }
                         <br></br>
