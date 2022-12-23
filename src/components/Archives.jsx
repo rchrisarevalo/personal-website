33b7fcc407974d2fb19466de3d4fecf5741c1cae
rchrisarevalo <rubenplayer105@gmail.com>
2022-12-22 23:13:48 -0600

    Table will automatically update through set times

--- src/components/Archives.jsx
+++ src/components/Archives.jsx
@@ -53,82 +53,162 @@ const Archives = () => {
                             <tr>
                                 <td>August 2021</td>
                                 <td>December 31, 2024</td>
-                                <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                {2024 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2024 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>September 2021</td>
                                 <td>December 31, 2024</td>
-                                <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                {2024 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2024 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>October 2021</td>
                                 <td>December 31, 2024</td>
-                                <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                {2024 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2024 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>November 2021</td>
                                 <td>December 31, 2024</td>
-                                <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                {2024 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2024 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>December 2021</td>
                                 <td>December 31, 2024</td>
-                                <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                {2024 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2024 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2024 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>January 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>February 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>March 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>April 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>May 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>June 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>July 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>August 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>September 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>October 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                             <tr>
                                 <td>November 2022</td>
                                 <td>December 31, 2025</td>
-                                <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                {2025 - new Date().getFullYear() > 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} years left`}</td>
+                                }
+                                {2025 - new Date().getFullYear() === 1 &&
+                                    <td>{`${2025 - new Date().getFullYear()} year left`}</td>
+                                }
                             </tr>
                         </table>
                     </div>
