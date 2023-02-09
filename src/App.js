9c81b434dc6f900217871597f8af014c1cf40115
rchrisarevalo <rubenplayer105@gmail.com>
2023-02-09 05:42:29 -0600

    Archive routes reconfigured.

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ import October2022Posts from './components/2022 Posts Components/October2022Post
 import November2022Posts from './components/2022 Posts Components/November2022Posts.jsx';
 import December2022Posts from './components/2022 Posts Components/December2022Posts.jsx';
 import January2023Posts from './components/2023 Posts Components/January2023Posts.jsx';
+import February2023Posts from './components/2023 Posts Components/February2023Posts.jsx';
 import Contact from './components/Contact.jsx';
 import Archives from "./components/Archives.jsx";
 import Settings from "./components/Settings.jsx";
@@ -99,8 +100,9 @@ function App() {
           {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/December" element={<December2022Posts />} />}
 
           {/* 2023 archive routes (expire on December 31, 2026) */}
+          {(Date.now() >= new Date(2023, 0, 1) && Date.now() <= new Date(2026, 11, 31)) && <Route path="/announcements/2023/January" element={<January2023Posts />} />}
           {(Date.now() >= new Date(db.archive[0].beginYear, db.archive[0].beginMonth - 1, db.archive[0].beginDate) && Date.now() <= new Date(db.archive[0].endYear, db.archive[0].endMonth - 1, db.archive[0].endDate)) &&
-            <Route path="/announcements/2023/January" element={<January2023Posts />} />
+            <Route path="/announcements/2023/February" element={<February2023Posts />} />
           }
 
           <Route path="/settings" element={<Settings />} />
