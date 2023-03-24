d5879c1861f2928e6aade82d36e73f50d81c3617
rchrisarevalo <rubenplayer105@gmail.com>
2023-03-24 13:46:51 -0500

    Error message updated.

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -3,13 +3,13 @@ import { Link, useNavigate } from "react-router-dom";
 
 import { IoWarningSharp } from "react-icons/io5"
 
-import nprogress from "nprogress";
-import "nprogress/nprogress.css";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Error404 = () => {
 
     const [seconds, setSeconds] = useState(new Date().getSeconds())
-    const [fiveSecondsCounter, setFiveSecondsCounter] = useState(1000000000)
+    const [fiveSecondsCounter, setFiveSecondsCounter] = useState(5)
     const nav = useNavigate()
 
     setTimeout(() => {
@@ -25,38 +25,61 @@ const Error404 = () => {
         }
     }, 1000)
 
-    useEffect(() => {
-        nprogress.configure({ minimum: 0.1, showSpinner: false, easing: 'ease', speed: 800, trickleSpeed: 200 });
-        nprogress.start();
-        nprogress.set(0.5);
-        nprogress.inc(0.5);
-        nprogress.done(true);
-
-        window.scrollTo(0, 0);
-    }, []);
-
-    return (
-        <div className="error-404-container">
-            <div className="error-404-message">
-                <IoWarningSharp size="7vh"></IoWarningSharp>
-                <h1>404</h1>
-                <br></br>
-                <p>Oops! It looks like what you were looking for does not exist on this website!</p>
-                <p>Here are some links below that can help you resolve this issue:</p>
-                <br></br>
-                <br></br>
-                <Link to="/about">About Me</Link>
-                <Link to="/announcements">Announcements</Link>
-                <Link to="/contact">Contact Me</Link>
-                <Link to="/archives">Archives</Link>
-                <Link to="/settings">Settings</Link>
-                <br></br>
-                <br></br>
-                <br></br>
-                <p>{`You will be redirected to the previous page in ${fiveSecondsCounter} seconds.`}</p>
+    if (!matchMedia('(max-width: 1024px)').matches){
+        return (
+            <div className="error-404-container">
+                <div className="error-404-display">
+                    <Row>
+                        <Col>
+                            <IoWarningSharp size="7vh"></IoWarningSharp>
+                            <h1>404</h1>
+                            <p>Oops! It looks like what you were looking for does not exist on this website!</p>
+                            <p>Here are some links below that can help you resolve this issue:</p>
+                        </Col>
+                        <Col>
+                            <li>
+                                <Link to="/about">About Me</Link>
+                                <Link to="/announcements">Announcements</Link>
+                                <Link to="/contact">Contact Me</Link>
+                                <Link to="/archives">Archives</Link>
+                                <Link to="/settings">Settings</Link>
+                            </li>
+                        </Col>
+                    </Row>
+                    <br></br>
+                    <br></br>
+                    <p>{`You will be redirected to the previous page in ${fiveSecondsCounter} seconds.`}</p>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className="error-404-container">
+                <div className="error-404-display">
+                    <Row>
+                        <Col>
+                            <IoWarningSharp size="7vh"></IoWarningSharp>
+                            <h1>404</h1>
+                            <p>Oops! It looks like what you were looking for does not exist on this website!</p>
+                            <p>Here are some links below that can help you resolve this issue:</p>
+                        </Col>
+                        <Col>
+                            <li>
+                                <Link to="/about">About Me</Link>
+                                <Link to="/announcements">Announcements</Link>
+                                <Link to="/contact">Contact Me</Link>
+                                <Link to="/archives">Archives</Link>
+                                <Link to="/settings">Settings</Link>
+                            </li>
+                        </Col>
+                    </Row>
+                    <br></br>
+                    <br></br>
+                    <p>{`You will be redirected to the previous page in ${fiveSecondsCounter} seconds.`}</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Error404;
