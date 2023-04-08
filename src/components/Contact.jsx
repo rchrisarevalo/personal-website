b6555f78cb5821937cb40be48e2ca4324383bb9d
rchrisarevalo <rubenplayer105@gmail.com>
2023-04-07 20:58:24 -0500

    Route reconfiguration

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { useLocation } from 'react-router-dom';
 
 import ContactForm from './ContactForm.jsx';
 import NavContact from '../NavContact.jsx';
@@ -10,10 +11,6 @@ import Col from 'react-bootstrap/Col'
 
 import nprogress from "nprogress";
 import "nprogress/nprogress.css";
-
-var current_mode = localStorage.getItem("d_l_mode")
-var dark_media_mode = localStorage.getItem("dark_media_theme")
-var light_media_mode = localStorage.getItem("light_media_theme")
 
 const Contact = () => {
 
@@ -27,35 +24,42 @@ const Contact = () => {
         window.scrollTo(0, 0);
     }, []);
 
-    return (
-        <div className="contact-page-container">
-            <Helmet>
-                <title>Ruben C. Arevalo - Personal Website - Contact Form</title>
-            </Helmet>
-            <NavContact />
-            <div className="contact-page-form">
-                <h1 id="contact-title">Contact Form</h1>
-                <br></br>
-                <div className="contact-description">
-                    <i id="contact-page-description">
-                        If you have any questions, complaints, or other concerns you might like to
-                        share, feel free to fill out the form below! However, do <b>NOT</b> attempt to send
-                        spam or other potentially dangerous messages.
-                    </i>
+    const [currentMode, setCurrentMode] = useState(localStorage.getItem("d_l_mode"))
+    const [darkMediaMode, setDarkMediaMode] = useState(localStorage.getItem("dark_media_theme"))
+    const [lightMediaMode, setLightMediaMode] = useState(localStorage.getItem("light_media_theme"))
+
+    var route = useLocation().pathname
+    localStorage.setItem("current_link", `${route}`)
+
+    if (currentMode === "default_dark" || currentMode === "dark" || (darkMediaMode === "true" && lightMediaMode === "false")) {
+        return (
+            <div className="contact-page-container">
+                <Helmet>
+                    <title>Ruben C. Arevalo - Personal Website - Contact Form</title>
+                </Helmet>
+                <NavContact />
+                <div className="contact-page-form">
+                    <h1 id="contact-title">Contact Form</h1>
+                    <br></br>
+                    <div className="contact-description">
+                        <i id="contact-page-description">
+                            If you have any questions, complaints, or other concerns you might like to
+                            share, feel free to fill out the form below! However, do <b>NOT</b> attempt to send
+                            spam or other potentially dangerous messages.
+                        </i>
+                        <br></br>
+                        <br></br>
+                        <i id="contact-page-description">
+                            Please fill out all the required fields. Otherwise, your message will not be sent when you
+                            attempt to submit the form.
+                        </i>
+                    </div>
                     <br></br>
                     <br></br>
-                    <i id="contact-page-description">
-                        Please fill out all the required fields. Otherwise, your message will not be sent when you
-                        attempt to submit the form.
-                    </i>
-                </div>
-                <br></br>
-                <br></br>
-                <ContactForm />
-                <br></br>
-                <br></br>
-                <Row id="contact-steps-row">
-                    {(current_mode === "default_dark" || current_mode === "dark" || (dark_media_mode === "true" && light_media_mode === "false")) &&
+                    <ContactForm />
+                    <br></br>
+                    <br></br>
+                    <Row id="contact-steps-row">
                         <div>
                             <Row id="contact-steps-row">
                                 <Col xs>
@@ -86,11 +90,46 @@ const Contact = () => {
                                 </Col>
                             </Row>
                         </div>
-                    }
-                    {(current_mode === "default_light" || current_mode === "light" || current_mode === "halloween"  || (dark_media_mode === "false" && light_media_mode === "true")) &&
+                    </Row>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <NewFooter />
+                </div>
+            </div>
+        )
+    } else if (currentMode === "default_light" || currentMode === "light" || currentMode === "halloween" || (darkMediaMode === "false" && lightMediaMode === "true")) {
+        return (
+            <div className="contact-page-container">
+                <Helmet>
+                    <title>Ruben C. Arevalo - Personal Website - Contact Form</title>
+                </Helmet>
+                <NavContact />
+                <div className="contact-page-form">
+                    <h1 id="contact-title">Contact Form</h1>
+                    <br></br>
+                    <div className="contact-description">
+                        <i id="contact-page-description">
+                            If you have any questions, complaints, or other concerns you might like to
+                            share, feel free to fill out the form below! However, do <b>NOT</b> attempt to send
+                            spam or other potentially dangerous messages.
+                        </i>
+                        <br></br>
+                        <br></br>
+                        <i id="contact-page-description">
+                            Please fill out all the required fields. Otherwise, your message will not be sent when you
+                            attempt to submit the form.
+                        </i>
+                    </div>
+                    <br></br>
+                    <br></br>
+                    <ContactForm />
+                    <br></br>
+                    <br></br>
+                    <Row id="contact-steps-row">
                         <div>
                             <Row id="contact-steps-row">
-                                <Col id="contact-steps-col">
+                                <Col xs>
                                     <figure id="contact-steps-dark">
                                         <div id="contact-steps-container">
                                             <h1><u>WHAT TO DO</u></h1>
@@ -99,11 +138,11 @@ const Contact = () => {
                                             <br></br>
                                             <li>Feel free to write me a message! However, please state it as clearly as you can. The reason for this is because vague messages can cause me, like others, to be confused, which will leave me unable to properly comprehend the details of your message.</li>
                                             <br></br>
-                                            <li>Ask any questions or concerns you might have about the website or the content! I am happy to answer to answer them as best as I can to address your concerns..</li>
+                                            <li>Ask any questions or concerns you might have about the website or the content! I am happy to answer to answer them as best as I can to address your concerns.</li>
                                         </div>
                                     </figure>
                                 </Col>
-                                <Col id="contact-steps-col">
+                                <Col xs>
                                     <figure id="contact-steps-dark">
                                         <div id="contact-steps-container">
                                             <h1><u>WHAT NOT TO DO</u></h1>
@@ -113,21 +152,21 @@ const Contact = () => {
                                             <li>Furthermore, messages of a discriminatory and/or violent nature are <b>PROHIBITED</b> and will not receive a reply whatsoever.</li>
                                             <br></br>
                                             <li>Do not try to send more than 10 messages a day unless it is a really long message that exceeds the character limit and requires you to enter another message.</li>
+                                            <br></br>
                                         </div>
                                     </figure>
                                 </Col>
                             </Row>
                         </div>
-                    }
-                </Row>
-                <br></br>
-                <br></br>
-                <br></br>
-                <NewFooter />
+                    </Row>
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <NewFooter />
+                </div>
             </div>
-
-        </div>
-    )
+        )
+    }
 }
 
 export default Contact;
