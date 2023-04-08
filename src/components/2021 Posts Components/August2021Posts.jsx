822235dcf7288dfe9a2cebbd83ea18bd2e77846b
rchrisarevalo <rubenplayer105@gmail.com>
2023-04-07 20:58:43 -0500

    Route reconfiguration

--- src/components/2021 Posts Components/August2021Posts.jsx
+++ src/components/2021 Posts Components/August2021Posts.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
 
@@ -12,6 +12,9 @@ const August2021Posts = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [])
+
+    var route = useLocation().pathname
+    localStorage.setItem("current_link", `${route}`)
 
     return (
         <div className="posts-main">
