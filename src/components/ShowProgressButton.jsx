72e2f93fe6feb4377807ed3cabb38e024bde2aa0
rchrisarevalo <rubenplayer105@gmail.com>
2023-05-12 16:01:29 -0500

    JSON.parse removed.

--- src/components/ShowProgressButton.jsx
+++ src/components/ShowProgressButton.jsx
@@ -3,24 +3,23 @@ import React, { useState } from 'react'
 const ShowProgressButton = () => {
 
     var show_progress_bar = localStorage.getItem("show_progress")
-    var retrieveCurrentState = JSON.parse(show_progress_bar)
-    const [status, setStatus] = useState(retrieveCurrentState)
+    const [status, setStatus] = useState(show_progress_bar)
 
     function handleHideProgress() {
         var show_choice = document.getElementById("show_check").value
     
         if (show_choice === "show") {
             localStorage.setItem("show_progress", "true")
-            setStatus(true)
+            setStatus("true")
         }
         else {
             localStorage.setItem("show_progress", "false")
-            setStatus(false)
+            setStatus("false")
         }
     }
 
     if (Date.now() < new Date(2023, 4, 13)) {
-        if (status === true) {
+        if (status === "true") {
             return (
                 <div>
                     <i>Hide progress bar feature <b>(available until May 13, 2023)</b>:</i>
@@ -32,7 +31,7 @@ const ShowProgressButton = () => {
                     </select>
                 </div>
             )
-        } else if (status === false) {
+        } else if (status === "false") {
             return (
                 <div>
                     <i>Hide progress bar feature <b>(available until May 13, 2023)</b>:</i>
