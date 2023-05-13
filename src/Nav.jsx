8a2830502c8b3250ce85ca6973ffc460616535ec
rchrisarevalo <rubenplayer105@gmail.com>
2023-05-12 22:11:07 -0500

    Component will be used for almost all pages.

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,46 +1,237 @@
 import React from "react";
-import { IoIosInformationCircleOutline } from "react-icons/io";
-import { IoSettingsOutline, IoMailOutline, IoMegaphoneOutline, IoArchiveOutline } from "react-icons/io5";
+import { IoIosInformationCircleOutline, IoIosInformationCircle } from "react-icons/io";
+import { IoSettingsOutline, IoSettingsSharp, IoMailOutline, IoMailOpenSharp, IoMegaphoneOutline, IoMegaphoneSharp, IoArchiveOutline, IoArchiveSharp } from "react-icons/io5";
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavMain = () => {
 
-  return (
-    // From React-Bootstrap
-    <Navbar expand="lg" variant="dark">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" id="menu" />
-        <div className="logo">
-          <Navbar.Brand href="./">Ruben C. Arevalo</Navbar.Brand>
-        </div>
-        <Link to="/settings"><IoSettingsOutline size="25px" id="setting-btn-icon" /></Link>
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav>
-            <Nav.Item>
-              <Nav.Link><Link to="/about"><IoIosInformationCircleOutline id="nav-icons-style" size="16px" />About Me</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link><Link to="/announcements"><IoMegaphoneOutline id="nav-icons-style" size="16px" />Announcements</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link><Link to="/contact"><IoMailOutline id="nav-icons-style" size="16px" />Contact Me</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link><Link to="/archives"><IoArchiveOutline id="nav-icons-style" size="16px" />Archives</Link></Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link><Link to="/settings"><IoSettingsOutline id="nav-icons-style" size="16px" />Settings</Link></Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+  var getRoute = useLocation().pathname
+
+  if (getRoute === "/about") {
+    return (
+      // From React-Bootstrap
+      <Navbar expand="lg" variant="dark">
+        <Container>
+          <Navbar.Toggle bg="light" aria-controls="basic-navbar-nav" />
+          <div className="logo">
+            <Navbar.Brand href="./">Ruben C. Arevalo</Navbar.Brand>
+          </div>
+          <Link to="/settings"><IoSettingsOutline size="25px" id="setting-btn-icon" /></Link>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Item id="current-link">
+                <Nav.Link><Link to="/about"><IoIosInformationCircle id="nav-icons-style" size="16px" /><b>About Me</b></Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/announcements"><IoMegaphoneOutline id="nav-icons-style" size="16px" />Announcements</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/contact"><IoMailOutline id="nav-icons-style" size="16px" />Contact Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/archives"><IoArchiveOutline id="nav-icons-style" size="16px" />Archives</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/settings"><IoSettingsOutline id="nav-icons-style" size="16px" />Settings</Link></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  } else if (getRoute === "/announcements") {
+    return (
+      <Navbar expand="lg" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <div className="logo">
+            <Navbar.Brand href="./">Ruben C. Arevalo</Navbar.Brand>
+          </div>
+          <Link to="/settings"><IoSettingsOutline size="25px" id="setting-btn-icon" /></Link>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link><Link to="/about"><IoIosInformationCircleOutline id="nav-icons-style" size="16px" />About Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item id="current-link">
+                <Nav.Link><Link to="/announcements"><IoMegaphoneSharp id="nav-icons-style" size="16px" /><b>Announcements</b></Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/contact"><IoMailOutline id="nav-icons-style" size="16px" />Contact Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/archives"><IoArchiveOutline id="nav-icons-style" size="16px" />Archives</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/settings"><IoSettingsOutline id="nav-icons-style" size="16px" />Settings</Link></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  } else if (getRoute === "/contact") {
+    return (
+      <Navbar expand="lg" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <div className="logo">
+            <Navbar.Brand href="./">Ruben C. Arevalo</Navbar.Brand>
+          </div>
+          <Link to="/settings"><IoSettingsOutline size="25px" id="setting-btn-icon" /></Link>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link><Link to="/about"><IoIosInformationCircleOutline id="nav-icons-style" size="16px" />About Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/announcements"><IoMegaphoneOutline id="nav-icons-style" size="16px" />Announcements</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item id="current-link">
+                <Nav.Link><Link to="/contact"><IoMailOpenSharp id="nav-icons-style" size="16px" /><b>Contact Me</b></Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/archives"><IoArchiveOutline id="nav-icons-style" size="16px" />Archives</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/settings"><IoSettingsOutline id="nav-icons-style" size="16px" />Settings</Link></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  } else if (getRoute === "/archives") {
+    return (
+      <Navbar expand="lg" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <div className="logo">
+            <Navbar.Brand href="./">Ruben C. Arevalo</Navbar.Brand>
+          </div>
+          <Link to="/settings"><IoSettingsOutline size="25px" id="setting-btn-icon" /></Link>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link><Link to="/about"><IoIosInformationCircleOutline id="nav-icons-style" size="16px" />About Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/announcements"><IoMegaphoneOutline id="nav-icons-style" size="16px" />Announcements</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/contact"><IoMailOutline id="nav-icons-style" size="16px" />Contact Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item id="current-link">
+                <Nav.Link><Link to="/archives"><IoArchiveSharp id="nav-icons-style" size="16px" /><b>Archives</b></Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/settings"><IoSettingsOutline id="nav-icons-style" size="16px" />Settings</Link></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  } else if (getRoute === "/archives") {
+    return (
+      <Navbar expand="lg" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <div className="logo">
+            <Navbar.Brand href="./">Ruben C. Arevalo</Navbar.Brand>
+          </div>
+          <Link to="/settings"><IoSettingsOutline size="25px" id="setting-btn-icon" /></Link>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link><Link to="/about"><IoIosInformationCircleOutline id="nav-icons-style" size="16px" />About Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/announcements"><IoMegaphoneOutline id="nav-icons-style" size="16px" />Announcements</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/contact"><IoMailOutline id="nav-icons-style" size="16px" />Contact Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item id="current-link">
+                <Nav.Link><Link to="/archives"><IoArchiveSharp id="nav-icons-style" size="16px" /><b>Archives</b></Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/settings"><IoSettingsOutline id="nav-icons-style" size="16px" />Settings</Link></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  } else if (getRoute === "/settings") {
+    return (
+      <Navbar expand="lg" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <div className="logo">
+            <Navbar.Brand href="./">Ruben C. Arevalo</Navbar.Brand>
+          </div>
+          <Link to="/settings"><IoSettingsSharp size="25px" id="setting-btn-icon" /></Link>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link><Link to="/about"><IoIosInformationCircleOutline id="nav-icons-style" size="16px" />About Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/announcements"><IoMegaphoneOutline id="nav-icons-style" size="16px" />Announcements</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/contact"><IoMailOutline id="nav-icons-style" size="16px" />Contact Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/archives"><IoArchiveOutline id="nav-icons-style" size="16px" />Archives</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item id="current-link">
+                <Nav.Link><Link to="/settings"><IoSettingsSharp id="nav-icons-style" size="16px" /><b>Settings</b></Link></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    )
+  } else {
+    return (
+      // From React-Bootstrap
+      <Navbar expand="lg" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" id="menu" />
+          <div className="logo">
+            <Navbar.Brand href="./">Ruben C. Arevalo</Navbar.Brand>
+          </div>
+          <Link to="/settings"><IoSettingsOutline size="25px" id="setting-btn-icon" /></Link>
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link><Link to="/about"><IoIosInformationCircleOutline id="nav-icons-style" size="16px" />About Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/announcements"><IoMegaphoneOutline id="nav-icons-style" size="16px" />Announcements</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/contact"><IoMailOutline id="nav-icons-style" size="16px" />Contact Me</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/archives"><IoArchiveOutline id="nav-icons-style" size="16px" />Archives</Link></Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link><Link to="/settings"><IoSettingsOutline id="nav-icons-style" size="16px" />Settings</Link></Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
 }
 
 export default NavMain;
