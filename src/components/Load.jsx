f808cd1da7f4e3508b6e3f158b33f4b2bfcd44be
rchrisarevalo <rubenplayer105@gmail.com>
2023-05-29 09:54:31 -0500

    Configured to close website for today.

--- src/components/Load.jsx
+++ src/components/Load.jsx
@@ -31,14 +31,14 @@ const Load = () => {
         setPercentage(percentage + 1)
     }, 20)
 
-    if (resStatus === 200 && Date.now() >= new Date(2023, 3, 8) && Date.now() < new Date(2023, 3, 10))
+    if (resStatus === 200 && Date.now() >= new Date(2023, 4, 29) && Date.now() < new Date(2023, 4, 30))
     {
         if (percentage === 100)
         {
             nav("/closed")
         }
     }
-    else if (resStatus === 200 && (Date.now() < new Date(2022, 3, 8) || Date.now() >= new Date(2023, 3, 10)))
+    else if (resStatus === 200 && (Date.now() < new Date(2022, 4, 29) || Date.now() >= new Date(2023, 4, 30)))
     {
         if (percentage === 100)
         {
