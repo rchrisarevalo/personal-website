1208d585a320615f73e29565c9c0d2f8b24b5cf8
rchrisarevalo <rubenplayer105@gmail.com>
2023-07-25 13:11:03 -0500

    Code refactoring.

--- src/components/ArchivesTable.jsx
+++ src/components/ArchivesTable.jsx
@@ -4,26 +4,26 @@ import db from './database/archives.json'
 const ArchivesTable = () => {
 
     const [themeState, setThemeState] = useState(localStorage.getItem("d_l_mode"))
+    var current_theme = localStorage.getItem("d_l_mode")
 
     useEffect(() => {
         setThemeState(localStorage.getItem("d_l_mode"))
-    })
+    }, [themeState, current_theme])
 
-    var generate_archive_information = db.archives.map(archive => 
+    var generate_archive_information = db.archives.map(archive =>
         <tr>
             <td>{`${archive["month"]} ${archive["beginYear"]}`}</td>
             <td>{`December 31, ${archive["endYear"]}`}</td>
-            { archive["endYear"] - new Date().getFullYear() > 1 && 
+            {archive["endYear"] - new Date().getFullYear() > 1 &&
                 <td>{`${archive["endYear"] - new Date().getFullYear()} years left`}</td>
             }
-            { archive["endYear"] - new Date().getFullYear() === 1 && 
+            {archive["endYear"] - new Date().getFullYear() === 1 &&
                 <td>{`${archive["endYear"] - new Date().getFullYear()} year left`}</td>
-            }        
+            }
         </tr>
     )
 
-    if (themeState === "default_dark" || themeState === "dark")
-    {
+    if (themeState === "default_dark" || themeState === "dark") {
         return (
             <div id="table-info-dark-mode">
                 <table>
@@ -32,7 +32,7 @@ const ArchivesTable = () => {
                         <th>Expiration Date</th>
                         <th>Time Left</th>
                     </tr>
-                    { generate_archive_information }
+                    {generate_archive_information}
                 </table>
             </div>
         )
@@ -45,7 +45,7 @@ const ArchivesTable = () => {
                         <th>Expiration Date</th>
                         <th>Time Left</th>
                     </tr>
-                    { generate_archive_information }
+                    {generate_archive_information}
                 </table>
             </div>
         )
