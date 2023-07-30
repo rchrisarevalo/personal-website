56d0d839b187c2370b8d1e81e4d4b2de3e017e8f
rchrisarevalo <rubenplayer105@gmail.com>
2023-07-30 12:52:25 -0500

    Light/dark mode hook bug fix in component.

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -45,6 +45,12 @@ const ContactForm = () => {
     const [darkMediaMode, setDarkMediaMode] = useState(localStorage.getItem("dark_media_theme"))
     const [lightMediaMode, setLightMediaMode] = useState(localStorage.getItem("light_media_theme"))
 
+    var current_theme = localStorage.getItem("d_l_mode")
+
+    useEffect(() => {
+        setCurrentMode(localStorage.getItem("d_l_mode"))
+    }, [current_theme])
+
     if (currentMode === "default_dark" || currentMode === "dark" || (darkMediaMode === "true" && lightMediaMode === "false")) {
         return (
             <div className="form-container">
