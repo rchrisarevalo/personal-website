1d8de87fa5a897f6c606f7190560690fa463a6ef
rchrisarevalo <rubenplayer105@gmail.com>
2023-09-11 04:30:39 -0500

    Code refactoring.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Route, Routes } from "react-router-dom";
 import AOS from 'aos';
 import "aos/dist/aos.css";
 
+import axios from "axios";
+
 // Components that will be used as page routes
 import Load from "./components/Load.jsx";
 import Intro from "./components/Intro.jsx";
@@ -22,19 +24,7 @@ import April2022Posts from './components/2022 Posts Components/April2022Posts.js
 import May2022Posts from './components/2022 Posts Components/May2022Posts.jsx';
 import June2022Posts from './components/2022 Posts Components/June2022Posts.jsx'
 import July2022Posts from './components/2022 Posts Components/July2022Posts.jsx';
-import August2022Posts from './components/2022 Posts Components/August2022Posts.jsx';
-import September2022Posts from './components/2022 Posts Components/September2022Posts.jsx';
-import October2022Posts from './components/2022 Posts Components/October2022Posts.jsx';
-import November2022Posts from './components/2022 Posts Components/November2022Posts.jsx';
-import December2022Posts from './components/2022 Posts Components/December2022Posts.jsx';
-import January2023Posts from './components/2023 Posts Components/January2023Posts.jsx';
-import February2023Posts from './components/2023 Posts Components/February2023Posts.jsx';
-import March2023Posts from './components/2023 Posts Components/March2023Posts.jsx';
-import April2023Posts from './components/2023 Posts Components/April2023Posts.jsx';
-import May2023Posts from './components/2023 Posts Components/May2023Posts.jsx';
-import June2023Posts from './components/2023 Posts Components/June2023Posts.jsx';
-import July2023Posts from './components/2023 Posts Components/July2023Posts.jsx';
-import August2023Posts from './components/2023 Posts Components/August2023Posts.jsx';
+import ArchivedPosts from './components/ArchivedPosts.jsx';
 import Contact from './components/Contact.jsx';
 import Archives from "./components/Archives.jsx";
 import WebsiteOperation from "./components/policies/WebsiteOperation.jsx";
@@ -42,7 +32,7 @@ import ArchivePolicy from "./components/policies/ArchivePolicy.jsx";
 import Error404 from "./components/Error404.jsx";
 import ClosedWeb from './components/ClosedWeb.jsx';
 
-import db from "./components/database/update.json";
+import db_archives from './components/database/archives.json'
 
 function App() {
 
@@ -52,6 +42,13 @@ function App() {
       once: true,
     });
     AOS.refresh();
+
+    axios.get("https://rchrisarevalo.github.io/personal-website").then((res) => {
+      console.log(res.status)
+    }).catch((error) => {
+      console.log(error.status)
+    })
+
   }, []);
 
   const [dateState, setDateState] = useState(new Date())
@@ -65,9 +62,14 @@ function App() {
     localStorage.removeItem("show_progress")
   }
 
+  // Filtered archive routes based on their availability.
+  var filtered_archive_routes = db_archives["archives"].filter(result => (Date.now() >= new Date(2022, 7)))
+  console.log(filtered_archive_routes)
+  var original_archive_routes = db_archives["archives"].map(result => <Route path={`/announcements/${result["beginYear"]}/${result["month"]}`} element=<ArchivedPosts monthNum={result["beginMonth"]} yearNum={result["beginYear"]} monthName={result["month"]} /> />)
+
   return (
     <div className="App">
-      {((Date.now() < new Date(2023, 7, 31, 0, 0)) || Date.now() >= new Date(2023, 8, 13, 0, 0)) &&
+      {((Date.now() < new Date(2023, 8, 14, 0, 0)) || Date.now() >= new Date(2023, 8, 17, 0, 0)) &&
         <Routes>
           <Route index path="/" element={<Load />} />
           <Route path="/about" element={<Intro />} />
@@ -91,30 +93,16 @@ function App() {
           {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/May" element={<May2022Posts />} />}
           {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/June" element={<June2022Posts />} />}
           {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/July" element={<July2022Posts />} />}
-          {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/August" element={<August2022Posts />} />}
-          {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/September" element={<September2022Posts />} />}
-          {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/October" element={<October2022Posts />} />}
-          {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/November" element={<November2022Posts />} />}
-          {(Date.now() >= new Date(2022, 0, 1) && Date.now() <= new Date(2025, 11, 31)) && <Route path="/announcements/2022/December" element={<December2022Posts />} />}
 
-          {/* 2023 archive routes (expire on December 31, 2026) */}
-          {(Date.now() >= new Date(2023, 0, 1) && Date.now() <= new Date(2026, 11, 31)) && <Route path="/announcements/2023/January" element={<January2023Posts />} />}
-          {(Date.now() >= new Date(2023, 0, 1) && Date.now() <= new Date(2026, 11, 31)) && <Route path="/announcements/2023/February" element={<February2023Posts />} />}
-          {(Date.now() >= new Date(2023, 0, 1) && Date.now() <= new Date(2026, 11, 31)) && <Route path="/announcements/2023/March" element={<March2023Posts />} />}
-          {(Date.now() >= new Date(2023, 0, 1) && Date.now() <= new Date(2026, 11, 31)) && <Route path="/announcements/2023/April" element={<April2023Posts />} />}
-          {(Date.now() >= new Date(2023, 0, 1) && Date.now() <= new Date(2026, 11, 31)) && <Route path="/announcements/2023/May" element={<May2023Posts />} />}
-          {(Date.now() >= new Date(2023, 0, 1) && Date.now() <= new Date(2026, 11, 31)) && <Route path="/announcements/2023/June" element={<June2023Posts />} />}
-          {(Date.now() >= new Date(2023, 0, 1) && Date.now() <= new Date(2026, 11, 31)) && <Route path="/announcements/2023/July" element={<July2023Posts />} />}
-          {(Date.now() >= new Date(db.archive[0].beginYear, db.archive[0].beginMonth - 1, db.archive[0].beginDate) && Date.now() <= new Date(db.archive[0].endYear, db.archive[0].endMonth - 1, db.archive[0].endDate)) &&
-            <Route path="/announcements/2023/August" element={<August2023Posts />} />
-          }
+          {/* 2022-2023 routes sorted. */}
+          {original_archive_routes}
 
           <Route path="/policies/site-operation" element={<WebsiteOperation />} />
           <Route path="/policies/archive" element={<ArchivePolicy />} />
           <Route path='*' element={<Error404 />} />
         </Routes>
       }
-      {Date.now() >= new Date(2023, 7, 31, 0, 0) && Date.now() < new Date(2023, 8, 13, 0, 0) &&
+      {Date.now() >= new Date(2023, 8, 14, 0, 0) && Date.now() < new Date(2023, 8, 17, 0, 0) &&
         <Routes>
           <Route index path="/" element={<Load />} />
           <Route path="/closed" element={<ClosedWeb />} />
