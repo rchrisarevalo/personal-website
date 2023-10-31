9a31f7fcab8fa91fa9e24ac67592357a703b6b71
rchrisarevalo <rubenplayer105@gmail.com>
2023-10-30 19:10:14 -0500

    Hiding web vitals.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
 
 // Rendering was update to accommodate new React.js version (v18.0.0 and greater)
@@ -23,4 +23,4 @@ rt_rend.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
